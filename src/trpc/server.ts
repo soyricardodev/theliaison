c74bc4ecb85343bda619ea0964640d8e1034271b
Ricardo Castro <soyricardodev@proton.me>
2024-04-24 14:18:18 -0400

    add supabase to context

--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -1,22 +1,38 @@
 import "server-only";
 
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 import { cache } from "react";
-
+import { createServerClient } from "@supabase/ssr";
 import { createCaller } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
+import { env } from "~/env";
 
 /**
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a tRPC call from a React Server Component.
  */
 const createContext = cache(() => {
-  const heads = new Headers(headers());
-  heads.set("x-trpc-source", "rsc");
+	const heads = new Headers(headers());
+	heads.set("x-trpc-source", "rsc");
 
-  return createTRPCContext({
-    headers: heads,
-  });
+	const cookieStore = cookies();
+
+	const supabase = createServerClient(
+		env.NEXT_PUBLIC_SUPABASE_URL,
+		env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+		{
+			cookies: {
+				get(name: string) {
+					return cookieStore.get(name)?.value;
+				},
+			},
+		},
+	);
+
+	return createTRPCContext({
+		supabase,
+		headers: heads,
+	});
 });
 
 export const api = createCaller(createContext);
