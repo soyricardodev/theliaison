399b6e7bfd3d51063a2059ca3d5d93357ff05524
Ricardo Castro <soyricardodev@proton.me>
2024-04-24 16:58:21 -0400

    fix error in auth supabase

--- src/app/(auth)/auth/callback/route.ts
+++ src/app/(auth)/auth/callback/route.ts
@@ -1,4 +1,3 @@
-import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 import { createClient } from "~/utils/supabase/server";
 
@@ -7,8 +6,7 @@ export async function GET(request: Request) {
 	const code = requestUrl.searchParams.get("code");
 
 	if (code) {
-		const cookieStore = cookies();
-		const supabase = createClient(cookieStore);
+		const supabase = createClient();
 		await supabase.auth.exchangeCodeForSession(code);
 	}
 
--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,12 +1,13 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
+import { env } from "~/env";
 
 export function createClient() {
 	const cookieStore = cookies();
 
 	return createServerClient(
-		process.env.NEXT_PUBLIC_SUPABASE_URL!,
-		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+		env.NEXT_PUBLIC_SUPABASE_URL,
+		env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 		{
 			cookies: {
 				get(name: string) {
