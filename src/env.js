4fb7317468a0b9c3a52373eb265cc39e17190ff0
Ricardo Castro <soyricardodev@proton.me>
2024-04-24 14:26:59 -0400

    fix: :bug: add DATABASE_URL to runtimeEnv for proper get the env

--- src/env.js
+++ src/env.js
@@ -30,7 +30,7 @@ export const env = createEnv({
 		NODE_ENV: process.env.NODE_ENV,
 		NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 		NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-		// NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+		DATABASE_URL: process.env.DATABASE_URL,
 	},
 	/**
 	 * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
