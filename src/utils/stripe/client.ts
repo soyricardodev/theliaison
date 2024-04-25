038b172d54d502e8765df49c153eba1d1675516b
Ricardo Castro <soyricardodev@proton.me>
2024-04-25 16:41:58 -0400

    add stripe utils

---
+++ src/utils/stripe/client.ts
@@ -0,0 +1,12 @@
+import { loadStripe, Stripe } from "@stripe/stripe-js";
+import { env } from "~/env";
+
+let stripePromise: Promise<Stripe | null>;
+
+export const getStripe = () => {
+	if (!stripePromise) {
+		stripePromise = loadStripe(env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+	}
+
+	return stripePromise;
+};
