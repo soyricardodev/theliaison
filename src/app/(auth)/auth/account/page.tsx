db828865faaeee8d51f314af8ce1043d6200f544
Ricardo Castro <soyricardodev@proton.me>
2024-04-25 16:42:32 -0400

    add account page to manage subscriptions

---
+++ src/app/(auth)/auth/account/page.tsx
@@ -0,0 +1,43 @@
+import CustomerPortalForm from "./_components/customer-portal-form";
+import { createClient } from "~/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+export default async function Account() {
+	const supabase = createClient();
+
+	const {
+		data: { user },
+	} = await supabase.auth.getUser();
+
+	const { data: subscription, error } = await supabase
+		.from("subscriptions")
+		.select("*, prices(*, products(*))")
+		.in("status", ["trialing", "active"])
+		.maybeSingle();
+
+	if (error) {
+		console.log(error);
+	}
+
+	if (!user) {
+		return redirect("/signin");
+	}
+
+	return (
+		<section className="mb-32 bg-black">
+			<div className="max-w-6xl px-4 py-8 mx-auto sm:px-6 sm:pt-24 lg:px-8">
+				<div className="sm:align-center sm:flex sm:flex-col">
+					<h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
+						Account
+					</h1>
+					<p className="max-w-2xl m-auto mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl">
+						We partnered with Stripe for a simplified billing.
+					</p>
+				</div>
+			</div>
+			<div className="p-4">
+				<CustomerPortalForm subscription={subscription} />
+			</div>
+		</section>
+	);
+}
