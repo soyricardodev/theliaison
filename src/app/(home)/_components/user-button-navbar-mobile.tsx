6a8578521c31e227f6e826f3623d9a0981fdc668
Ricardo Castro <soyricardodev@proton.me>
2024-04-29 11:17:12 -0400

    add user button navbar for mobile

---
+++ src/app/(home)/_components/user-button-navbar-mobile.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { motion } from "framer-motion";
+import { buttonVariants } from "~/components/ui/button";
+import { cn } from "~/lib/utils";
+
+import { useEffect, useState } from "react";
+import { createClient } from "~/utils/supabase/client";
+import { User } from "@supabase/supabase-js";
+
+export function UserButtonNavbarMobile() {
+	const [user, setUser] = useState<User | null>(null);
+	const supabase = createClient();
+
+	const getUserData = async () => {
+		const {
+			data: { user },
+			error,
+		} = await supabase.auth.getUser();
+
+		if (error) return setUser(null);
+
+		setUser(user);
+	};
+
+	useEffect(() => {
+		getUserData();
+	}, []);
+
+	return (
+		<motion.a
+			href={user ? "/profile" : "/auth/signin"}
+			initial={{ x: "125%" }}
+			animate={{ x: "0" }}
+			exit={{
+				x: "125%",
+				transition: { ease: "easeOut", duration: 2.2 },
+			}}
+			transition={{ ease: "easeOut", duration: 0.5 }}
+			className={cn(buttonVariants())}
+		>
+			{user ? "Dashboard" : "Get Started"}
+		</motion.a>
+	);
+}
