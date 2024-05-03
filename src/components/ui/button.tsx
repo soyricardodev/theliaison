3716af463a4f119d0d53a4ccd9a3d4b1d5b7ff49
Ricardo Castro <soyricardodev@proton.me>
2024-05-03 12:49:07 -0400

    style: :lipstick: change from text-base to text-sm
    looks better with the default text-xs

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import * as React from "react";
 import { cn } from "~/lib/utils";
 
 const buttonVariants = cva(
-	"inline-flex items-center justify-center whitespace-nowrap text-base font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 rounded-full relative gap-x-0.5 cursor-pointer",
+	"inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 rounded-full relative gap-x-0.5 cursor-pointer",
 	{
 		variants: {
 			variant: {
@@ -20,9 +20,9 @@ const buttonVariants = cva(
 				link: "text-primary underline-offset-4 hover:underline",
 			},
 			size: {
-				default: "h-10 px-4 py-2",
-				sm: "h-9 rounded-md px-3",
-				lg: "h-11 rounded-md px-8",
+				default: "h-8 px-3 py-2",
+				sm: "h-9 px-3",
+				lg: "h-11 px-8",
 				icon: "h-10 w-10",
 			},
 		},
