14ac549087ed06957290d5720605a2a547bd94b4
Ricardo Castro <soyricardodev@proton.me>
2024-05-03 13:45:18 -0400

    feat: :sparkles: add tailwind typography plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -89,6 +89,7 @@ const config = {
 		},
 	},
 	plugins: [
+		require("@tailwindcss/typography"),
 		require("tailwindcss-animate"),
 		addVariablesForColors,
 		// biome-ignore lint/suspicious/noExplicitAny: missing types
