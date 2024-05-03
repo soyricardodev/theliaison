e5d5d6e0e687b9d24942df7795d5e027f67e5795
Ricardo Castro <soyricardodev@proton.me>
2024-05-03 15:24:49 -0400

    temporary pricing page in maintenance mode

--- src/app/(home)/pricing/page.tsx
+++ src/app/(home)/pricing/page.tsx
@@ -26,11 +26,27 @@ export default async function PricingPage() {
 		.order("metadata->index")
 		.order("unit_amount", { referencedTable: "prices" });
 
+	// <Pricing
+	// 	user={user}
+	// 	products={products ?? []}
+	// 	subscription={subscription}
+	// />
 	return (
-		<Pricing
-			user={user}
-			products={products ?? []}
-			subscription={subscription}
-		/>
+		<main className="flex-1 overflow-auto">
+			<div className="w-full">
+				<div className="mx-auto flex max-w-[1160px] flex-col gap-8 px-5 pt-10 md:gap-12 md:pt-16">
+					<div className="grid justify-items-center gap-2 md:gap-4">
+						<h1 className="text-center text-2xl font-semibold tracking-tighter md:text-5xl">
+							Simple pricing for everyone.
+						</h1>
+						<p className="max-w-md text-center text-sm text-[#666666] md:max-w-xl md:text-base text-pretty">
+							We want to empower builders of all sizes to create beautiful
+							interfaces; from individuals to big enterprises, we have a plan
+							that fits just what you need.
+						</p>
+					</div>
+				</div>
+			</div>
+		</main>
 	);
 }
