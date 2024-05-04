4c60097262e7d98d31186d50b71c70eab49f05fa
Ricardo Castro <soyricardodev@proton.me>
2024-05-04 14:45:31 -0400

    style: :lipstick: format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "~/styles/globals.css";
-import { GeistSans } from "geist/font/sans";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { GeistSans } from "geist/font/sans";
 
 import { Footer } from "~/components/footer";
 import { Header } from "~/components/header";
