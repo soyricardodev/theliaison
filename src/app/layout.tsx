45d602472085c5bb2c0fce0eaffa2d525c3eb337
Ricardo Castro <soyricardodev@proton.me>
2024-05-04 13:39:15 -0400

    fix: :bug: change analytics import from react to next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "~/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import { Footer } from "~/components/footer";
