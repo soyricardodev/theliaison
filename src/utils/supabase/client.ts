c5df7f86e43c918895ae9a8444fb60a579590d55
soyricardodev <soyricardodev@proton.me>
2024-05-11 16:56:26 -0400

    add t3env

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,8 +1,9 @@
 import { createBrowserClient } from "@supabase/ssr";
 import type { Database } from "~/types/database-types";
+import { env } from "~/env";
 
 export const createClient = () =>
 	createBrowserClient<Database>(
-		process.env.NEXT_PUBLIC_SUPABASE_URL!,
-		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+		env.NEXT_PUBLIC_SUPABASE_URL,
+		env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 	);
