c772e11dfea505d65127aef2dfd0921b10b6cd83
soyricardodev <soyricardodev@proton.me>
2024-05-11 16:51:26 -0400

    add t3env to supabase config

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -1,5 +1,6 @@
 import { type CookieOptions, createServerClient } from "@supabase/ssr";
 import { type NextRequest, NextResponse } from "next/server";
+import { env } from "~/env";
 
 export async function updateSession(request: NextRequest) {
 	let response = NextResponse.next({
@@ -9,8 +10,8 @@ export async function updateSession(request: NextRequest) {
 	});
 
 	const supabase = createServerClient(
-		process.env.NEXT_PUBLIC_SUPABASE_URL!,
-		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+		env.NEXT_PUBLIC_SUPABASE_URL,
+		env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 		{
 			cookies: {
 				get(name: string) {
