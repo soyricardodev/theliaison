b6626f28bfd3d1b96a76446649d5fc35bb6b239d
soyricardodev <soyricardodev@proton.me>
2024-05-12 00:50:52 -0400

    add new signup schema

--- src/utils/validators/auth.ts
+++ src/utils/validators/auth.ts
@@ -44,3 +44,25 @@ export const signUpSchema = z.object({
 });
 
 export type SignUp = z.infer<typeof signUpSchema>;
+
+export const simpleSignUpSchema = z.object({
+	email: z
+		.string({ required_error: "Please enter your email" })
+		.email({ message: "Email is invalid." }),
+	password: z.string().min(8, {
+		message: "Password must be at least 8 characters.",
+	}),
+	name: z
+		.string({ required_error: "Please enter your full name" })
+		.min(2, { message: "Your name should be at least 2 characters" }),
+	username: z
+		.string()
+		.regex(/^[a-zA-Z0-9_]+$/, {
+			message: "Username must only contain letters, numbers, and underscores.",
+		})
+		.min(3, {
+			message: "Username must be at least 3 characters.",
+		}),
+});
+
+export type SimpleSignUp = z.infer<typeof simpleSignUpSchema>;
