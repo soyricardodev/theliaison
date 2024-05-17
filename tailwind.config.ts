305b86093fb8280ab24e07b7ac40851e6ee3e1f8
soyricardodev <soyricardodev@proton.me>
2024-05-17 10:02:55 -0400

    feat: :sparkles: remove shadcn theme and add gradient animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,5 @@
 import type { Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
-import svgToDataUri from "mini-svg-data-uri";
-
-// @ts-expect-error missing types
-import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
 import { nextui } from "@nextui-org/react";
 
 const config = {
@@ -12,83 +8,20 @@ const config = {
 		"./src/**/*.{ts,tsx}",
 		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
 	],
-	prefix: "",
 	theme: {
-		container: {
-			center: true,
-			padding: "2rem",
-			screens: {
-				"2xl": "1400px",
-			},
-		},
 		extend: {
-			colors: {
-				border: "hsl(var(--border))",
-				input: "hsl(var(--input))",
-				ring: "hsl(var(--ring))",
-				background: "hsl(var(--background))",
-				foreground: "hsl(var(--foreground))",
-				primary: {
-					DEFAULT: "hsl(var(--primary))",
-					foreground: "hsl(var(--primary-foreground))",
-				},
-				// secondary: {
-				// 	DEFAULT: "hsl(var(--secondary))",
-				// 	foreground: "hsl(var(--secondary-foreground))",
-				// },
-				destructive: {
-					DEFAULT: "hsl(var(--destructive))",
-					foreground: "hsl(var(--destructive-foreground))",
-				},
-				muted: {
-					DEFAULT: "hsl(var(--muted))",
-					foreground: "hsl(var(--muted-foreground))",
-				},
-				accent: {
-					DEFAULT: "hsl(var(--accent))",
-					foreground: "hsl(var(--accent-foreground))",
-				},
-				popover: {
-					DEFAULT: "hsl(var(--popover))",
-					foreground: "hsl(var(--popover-foreground))",
-				},
-				card: {
-					DEFAULT: "hsl(var(--card))",
-					foreground: "hsl(var(--card-foreground))",
-				},
-			},
-			borderRadius: {
-				lg: "var(--radius)",
-				md: "calc(var(--radius) - 2px)",
-				sm: "calc(var(--radius) - 4px)",
-			},
-			keyframes: {
-				"accordion-down": {
-					from: { height: "0" },
-					to: { height: "var(--radix-accordion-content-height)" },
-				},
-				"accordion-up": {
-					from: { height: "var(--radix-accordion-content-height)" },
-					to: { height: "0" },
-				},
-				marquee: {
-					from: { transform: "translateX(0)" },
-					to: { transform: "translateX(calc(-100% - var(--gap)))" },
-				},
-				"marquee-vertical": {
-					from: { transform: "translateY(0)" },
-					to: { transform: "translateY(calc(-100% - var(--gap)))" },
-				},
+			fontFamily: {
+				sans: ["var(--font-geist-sans)", ...fontFamily.sans],
 			},
 			animation: {
-				"accordion-down": "accordion-down 0.2s ease-out",
-				"accordion-up": "accordion-up 0.2s ease-out",
-				marquee: "marquee var(--duration) linear infinite",
-				"marquee-vertical": "marquee-vertical var(--duration) linear infinite",
+				gradient: "gradient 8s linear infinite",
 			},
-			fontFamily: {
-				heading: ["var(--font-heading)", ...fontFamily.serif],
-				sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+			keyframes: {
+				gradient: {
+					to: {
+						backgroundPosition: "var(--bg-size) 0",
+					},
+				},
 			},
 		},
 	},
@@ -96,46 +29,7 @@ const config = {
 		require("@tailwindcss/typography"),
 		require("tailwindcss-animate"),
 		nextui(),
-		addVariablesForColors,
-		// biome-ignore lint/suspicious/noExplicitAny: missing types
-		({ matchUtilities, theme }: any) => {
-			matchUtilities(
-				{
-					"bg-grid": (value: string) => ({
-						backgroundImage: `url("${svgToDataUri(
-							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
-						)}")`,
-					}),
-					"bg-grid-small": (value: string) => ({
-						backgroundImage: `url("${svgToDataUri(
-							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
-						)}")`,
-					}),
-					"bg-dot": (value: string) => ({
-						backgroundImage: `url("${svgToDataUri(
-							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`,
-						)}")`,
-					}),
-				},
-				{
-					values: flattenColorPalette(theme("backgroundColor")),
-					type: "color",
-				},
-			);
-		},
 	],
 } satisfies Config;
 
 export default config;
-
-// biome-ignore lint/suspicious/noExplicitAny: missing types
-function addVariablesForColors({ addBase, theme }: any) {
-	const allColors = flattenColorPalette(theme("colors"));
-	const newVars = Object.fromEntries(
-		Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
-	);
-
-	addBase({
-		":root": newVars,
-	});
-}
