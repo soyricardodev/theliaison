639e36382236682502074554926bdd3532d50c91
soyricardodev <soyricardodev@proton.me>
2024-05-17 15:37:29 -0400

    improve ui in individual poll

--- src/app/(polling)/poll/[id]/option-to-vote.tsx
+++ src/app/(polling)/poll/[id]/option-to-vote.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
-import { Progress } from "~/components/ui/progress";
+import { Progress, Tooltip } from "@nextui-org/react";
 import { cn } from "~/lib/utils";
 import { createClient } from "~/utils/supabase/client";
 
@@ -49,28 +49,30 @@ export function OptionToVote({
 	}
 
 	return (
-		<li
-			className={cn(
-				" px-3 py-2 rounded-lg transition-colors cursor-crosshair",
-				alreadyVoted && "hover:bg-gray-200 cursor-not-allowed",
-				optionVotedId === option.id &&
-					"bg-gray-300 hover:bg-gray-200 cursor-crosshair",
-			)}
-			onClick={vote}
-			onKeyDown={vote}
-		>
-			<div className="flex items-center gap-2">
-				<div className="flex items-center justify-center mr-4">
-					<span className="text-sm md:text-base">{option.votes}</span>
-				</div>
-				<div className="flex flex-col w-full gap-3">
-					<p className="text-sm md:text-lg font-medium">{option.text}</p>
-					<Progress value={option.percentage} className="h-[10px]" />
-					<p className="text-base text-right text-gray-500">
-						{option.percentage}%
-					</p>
-				</div>
-			</div>
-		</li>
+		<Tooltip content={<p>Click to vote</p>} offset={-7}>
+			<button
+				type="button"
+				className={cn(
+					"py-4 px-5 rounded-lg transition-colors bg-content1 hover:bg-content2 shadow-large",
+					alreadyVoted && "hover:bg-gray-200 cursor-not-allowed",
+					optionVotedId === option.id &&
+						"bg-gray-300 hover:bg-gray-200 cursor-crosshair",
+				)}
+				onClick={vote}
+				onKeyDown={vote}
+			>
+				<Progress
+					value={option.percentage}
+					showValueLabel
+					label={<p>{option.text}</p>}
+					classNames={{
+						track: "drop-shadow-md border border-default",
+						label: "tracking-wider font-medium text-default-600",
+						value: "text-foreground/60",
+					}}
+					color="secondary"
+				/>
+			</button>
+		</Tooltip>
 	);
 }
