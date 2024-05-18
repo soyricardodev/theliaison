a883e63d63b62e7fdc3c2d22c2465088eaebb88a
soyricardodev <soyricardodev@proton.me>
2024-05-18 00:44:44 -0400

    add modal to login user when try to vote

--- src/app/(polling)/poll/[id]/option-to-vote.tsx
+++ src/app/(polling)/poll/[id]/option-to-vote.tsx
@@ -1,16 +1,34 @@
 "use client";
 
-import { Progress, Tooltip } from "@nextui-org/react";
+import {
+	Progress,
+	Tooltip,
+	Modal,
+	ModalContent,
+	ModalHeader,
+	ModalBody,
+	ModalFooter,
+	Button,
+	useDisclosure,
+	Input,
+	Link,
+} from "@nextui-org/react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { toast } from "sonner";
+import { login } from "~/app/(auth)/actions";
 import { cn } from "~/lib/utils";
 import { createClient } from "~/utils/supabase/client";
 
 type OptionToVote = {
 	canVote: boolean;
 	pollId: number;
-	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-	option: any;
+	option: {
+		votes: number | undefined;
+		percentage: number | undefined;
+		id: number;
+		text: string;
+	};
 	alreadyVoted: boolean;
 	optionVotedId?: number;
 	userId?: string;
@@ -24,10 +42,16 @@ export function OptionToVote({
 	optionVotedId,
 	userId,
 }: OptionToVote) {
+	const { isOpen, onOpen, onOpenChange } = useDisclosure();
+	const [isLoading, setIsLoading] = useState(false);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 	const supabase = createClient();
 	const router = useRouter();
 
 	async function vote() {
+		if (!userId) return onOpen();
+
 		if (alreadyVoted) {
 			return toast.error("You have already voted.");
 		}
@@ -48,31 +72,143 @@ export function OptionToVote({
 		}
 	}
 
+	const optionVotedByUser = optionVotedId === option.id;
+
 	return (
-		<Tooltip content={<p>Click to vote</p>} offset={-7}>
-			<button
-				type="button"
-				className={cn(
-					"py-4 px-5 rounded-lg transition-colors bg-content1 hover:bg-content2 shadow-large",
-					alreadyVoted && "hover:bg-gray-200 cursor-not-allowed",
-					optionVotedId === option.id &&
-						"bg-gray-300 hover:bg-gray-200 cursor-crosshair",
-				)}
-				onClick={vote}
-				onKeyDown={vote}
+		<>
+			<Tooltip
+				content={
+					<p>{optionVotedByUser ? "This is your vote" : "Click to vote"}</p>
+				}
 			>
-				<Progress
-					value={option.percentage}
-					showValueLabel
-					label={<p>{option.text}</p>}
-					classNames={{
-						track: "drop-shadow-md border border-default",
-						label: "tracking-wider font-medium text-default-600",
-						value: "text-foreground/60",
-					}}
-					color="secondary"
-				/>
-			</button>
-		</Tooltip>
+				<button
+					type="button"
+					className={cn(
+						"py-4 px-5 rounded-lg transition-colors bg-content1 hover:bg-content2 shadow-large",
+						optionVotedByUser && "bg-content2 cursor-not-allowed",
+					)}
+					onClick={vote}
+				>
+					<Progress
+						value={option.percentage}
+						showValueLabel
+						label={<p>{option.text}</p>}
+						classNames={{
+							track: "drop-shadow-md border border-default",
+							label: "tracking-wider font-medium text-default-600",
+							value: "text-foreground/60",
+						}}
+						color="secondary"
+					/>
+				</button>
+			</Tooltip>
+			<Modal
+				isOpen={isOpen}
+				onOpenChange={onOpenChange}
+				placement="top-center"
+				className="dark"
+				backdrop="blur"
+			>
+				<ModalContent>
+					{(onClose) => (
+						<>
+							<ModalHeader className="flex flex-col gap-1 text-default-900">
+								You need to log in to vote
+							</ModalHeader>
+							<ModalBody>
+								<Input
+									autoFocus
+									endContent={
+										<MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+									}
+									label="Email"
+									placeholder="Enter your email"
+									variant="bordered"
+									value={email}
+									onChange={(e) => setEmail(e.target.value)}
+									required
+									isRequired
+								/>
+								<Input
+									endContent={
+										<LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+									}
+									label="Password"
+									placeholder="Enter your password"
+									type="password"
+									variant="bordered"
+									value={password}
+									onChange={(e) => setPassword(e.target.value)}
+									required
+									isRequired
+								/>
+								<div className="flex py-2 px-1 justify-end">
+									<Link color="primary" href="/signup" size="sm">
+										Dont have an account? Sign Up
+									</Link>
+								</div>
+							</ModalBody>
+							<ModalFooter>
+								<Button color="danger" variant="flat" onPress={onClose}>
+									Close
+								</Button>
+								<Button
+									color="primary"
+									disabled={isLoading}
+									isDisabled={isLoading}
+									isLoading={isLoading}
+									onPress={() => {
+										setIsLoading(true);
+										login({ email, password });
+									}}
+								>
+									Sign in
+								</Button>
+							</ModalFooter>
+						</>
+					)}
+				</ModalContent>
+			</Modal>
+		</>
 	);
 }
+
+export const MailIcon = (props: React.SVGProps<SVGSVGElement>) => (
+	<svg
+		aria-hidden="true"
+		fill="none"
+		focusable="false"
+		height="1em"
+		role="presentation"
+		viewBox="0 0 24 24"
+		width="1em"
+		{...props}
+	>
+		<path
+			d="M17 3.5H7C4 3.5 2 5 2 8.5V15.5C2 19 4 20.5 7 20.5H17C20 20.5 22 19 22 15.5V8.5C22 5 20 3.5 17 3.5ZM17.47 9.59L14.34 12.09C13.68 12.62 12.84 12.88 12 12.88C11.16 12.88 10.31 12.62 9.66 12.09L6.53 9.59C6.21 9.33 6.16 8.85 6.41 8.53C6.67 8.21 7.14 8.15 7.46 8.41L10.59 10.91C11.35 11.52 12.64 11.52 13.4 10.91L16.53 8.41C16.85 8.15 17.33 8.2 17.58 8.53C17.84 8.85 17.79 9.33 17.47 9.59Z"
+			fill="currentColor"
+		/>
+	</svg>
+);
+
+export const LockIcon = (props: React.SVGProps<SVGSVGElement>) => (
+	<svg
+		aria-hidden="true"
+		fill="none"
+		focusable="false"
+		height="1em"
+		role="presentation"
+		viewBox="0 0 24 24"
+		width="1em"
+		{...props}
+	>
+		<path
+			d="M12.0011 17.3498C12.9013 17.3498 13.6311 16.6201 13.6311 15.7198C13.6311 14.8196 12.9013 14.0898 12.0011 14.0898C11.1009 14.0898 10.3711 14.8196 10.3711 15.7198C10.3711 16.6201 11.1009 17.3498 12.0011 17.3498Z"
+			fill="currentColor"
+		/>
+		<path
+			d="M18.28 9.53V8.28C18.28 5.58 17.63 2 12 2C6.37 2 5.72 5.58 5.72 8.28V9.53C2.92 9.88 2 11.3 2 14.79V16.65C2 20.75 3.25 22 7.35 22H16.65C20.75 22 22 20.75 22 16.65V14.79C22 11.3 21.08 9.88 18.28 9.53ZM12 18.74C10.33 18.74 8.98 17.38 8.98 15.72C8.98 14.05 10.34 12.7 12 12.7C13.66 12.7 15.02 14.06 15.02 15.72C15.02 17.39 13.67 18.74 12 18.74ZM7.35 9.44C7.27 9.44 7.2 9.44 7.12 9.44V8.28C7.12 5.35 7.95 3.4 12 3.4C16.05 3.4 16.88 5.35 16.88 8.28V9.45C16.8 9.45 16.73 9.45 16.65 9.45H7.35V9.44Z"
+			fill="currentColor"
+		/>
+	</svg>
+);
