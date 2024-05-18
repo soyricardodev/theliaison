a8fcf5014f0c054d10ae5db94e411fc4838c4a9e
soyricardodev <soyricardodev@proton.me>
2024-05-17 23:45:58 -0400

    add reactCompiler experimental flag

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
+	experimental: {
+		reactCompiler: true,
+	},
 	typescript: {
 		ignoreBuildErrors: true,
 	},
