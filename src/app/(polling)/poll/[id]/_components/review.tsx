14b61d97165418753a4aa88b891e8d1091688bb4
soyricardodev <soyricardodev@proton.me>
2024-05-17 23:35:26 -0400

    improve review ui

--- src/app/(polling)/poll/[id]/_components/review.tsx
+++ src/app/(polling)/poll/[id]/_components/review.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import React from "react";
-import { User } from "@nextui-org/react";
+import { Tooltip, User } from "@nextui-org/react";
 import { Icon } from "@iconify/react";
-
-import { cn } from "~/lib/utils";
 
 export type ReviewType = {
 	user: {
@@ -32,34 +30,24 @@ const Review = React.forwardRef<HTMLDivElement, ReviewProps>(
 							name: "font-medium",
 							description: "text-small",
 						}}
-						description={new Intl.DateTimeFormat("en-US", {
-							month: "long",
-							day: "numeric",
-							year: "numeric",
-						}).format(new Date(createdAt))}
+						description={`commented ${createdAt}`}
 						name={user.name}
 					/>
 				</div>
 				<div className="flex items-center gap-1">
-					{Array.from({ length: 5 }, (_, i) => {
-						const isSelected = i + 1 <= rating;
-
-						return (
-							<Icon
-								key={`${rating}-${i}`}
-								className={cn(
-									"text-lg sm:text-xl",
-									isSelected ? "text-warning" : "text-default-200",
-								)}
-								icon="solar:star-bold"
-							/>
-						);
-					})}
+					<Tooltip content="Upvote">
+						<Icon icon="solar:round-arrow-up-bold-duotone" className="size-8" />
+					</Tooltip>
+					<Tooltip content="Downvote">
+						<Icon
+							icon="solar:round-arrow-down-bold-duotone"
+							className="size-8"
+						/>
+					</Tooltip>
 				</div>
 			</div>
 			<div className="mt-4 w-full">
-				<p className="font-medium text-default-900">{title}</p>
-				<p className="mt-2 text-default-500">{content || children}</p>
+				<p className="text-default-900">{content || children}</p>
 			</div>
 		</div>
 	),
