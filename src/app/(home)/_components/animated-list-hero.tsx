15ec1c063ae709859c6c95d558799f93a105fc15
soyricardodev <soyricardodev@proton.me>
2024-05-19 15:21:08 -0400

    fix: move key before props

--- src/app/(home)/_components/animated-list-hero.tsx
+++ src/app/(home)/_components/animated-list-hero.tsx
@@ -85,7 +85,7 @@ export default function AnimatedListHero() {
 		<div className="relative flex max-h-[400px] min-h-[400px] w-full max-w-[32rem] flex-col overflow-hidden rounded-lg border bg-background p-6 shadow-lg">
 			<AnimatedList>
 				{notifications.map((item, idx) => (
-					<Notification {...item} key={idx} />
+					<Notification key={`notification-${item.name}-${idx}`} {...item} />
 				))}
 			</AnimatedList>
 		</div>
