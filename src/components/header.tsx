8c8a9c38cac4e4394054fc2e4be757e1012b00c0
soyricardodev <soyricardodev@proton.me>
2024-05-20 11:02:37 -0400

    feat: remove unused imports and move logic to other files

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,6 @@
-"use client";
-
-import { Icon } from "@iconify/react";
 import {
-	Avatar,
-	Badge,
 	BreadcrumbItem,
 	Breadcrumbs,
-	Button,
-	Dropdown,
-	DropdownItem,
-	DropdownMenu,
-	DropdownTrigger,
 	Link,
 	Navbar,
 	NavbarBrand,
@@ -21,78 +11,16 @@ import React from "react";
 
 import { Logo } from "./logo";
 
-import type { User } from "@supabase/supabase-js";
+import { HeaderUser } from "./header-user";
 
 interface HeaderProps {
 	breadcrumbs?: Array<{
 		name: string;
 		href: string;
 	}>;
-	user?: User | null;
 }
 
-const menuItemsLogout = [
-	{
-		key: "login",
-		label: "Login",
-		href: "/login",
-		Icon: <Icon icon="solar:user-rounded-bold-duotone" width={24} />,
-	},
-	{
-		key: "services",
-		label: "Services",
-		href: "/services",
-		Icon: <Icon icon="solar:three-squares-bold-duotone" width={24} />,
-	},
-	{
-		key: "faq",
-		label: "FAQs",
-		href: "/faq",
-		Icon: <Icon icon="solar:question-circle-bold-duotone" width={24} />,
-	},
-	{
-		key: "pricing",
-		label: "Pricing",
-		href: "/pricing",
-		Icon: <Icon icon="solar:dollar-bold-duotone" width={24} />,
-	},
-];
-
-const menuItemsLoggedIn = (username: string) => [
-	{
-		key: "explore",
-		label: "Explore",
-		href: "/explore",
-		Icon: <Icon icon="solar:three-squares-bold-duotone" width={24} />,
-	},
-	{
-		key: "profile",
-		label: "Profile",
-		href: `/${username}`,
-		Icon: <Icon icon="solar:user-rounded-bold-duotone" width={24} />,
-	},
-	{
-		key: "faq",
-		label: "FAQs",
-		href: "/faq",
-		Icon: <Icon icon="solar:question-circle-bold-duotone" width={24} />,
-	},
-	{
-		key: "subscription",
-		label: "Subscription & Billing",
-		href: "/subscription",
-		Icon: <Icon icon="solar:card-bold-duotone" width={24} />,
-	},
-	{
-		key: "pricing",
-		label: "Pricing",
-		href: "/pricing",
-		Icon: <Icon icon="solar:dollar-bold-duotone" width={24} />,
-	},
-];
-
-export function Header({ breadcrumbs, user }: HeaderProps) {
-	const menuLinks = user ? menuItemsLoggedIn("Ricardo") : menuItemsLogout;
+export function Header({ breadcrumbs }: HeaderProps) {
 	return (
 		<div className="w-full">
 			<Navbar
@@ -126,47 +54,7 @@ export function Header({ breadcrumbs, user }: HeaderProps) {
 				>
 					{/* User Menu */}
 					<NavbarItem className="px-2">
-						<Dropdown placement="bottom-end" className="dark text-default-900">
-							<DropdownTrigger>
-								{user != null ? (
-									<button
-										className="mt-1 h-8 w-8 transition-transform"
-										type="button"
-									>
-										<Badge
-											color="success"
-											content=""
-											placement="bottom-right"
-											shape="circle"
-										>
-											<Avatar
-												size="sm"
-												src="https://i.pravatar.cc/150?u=a04258114e29526708c"
-											/>
-										</Badge>
-									</button>
-								) : (
-									<Button isIconOnly variant="flat">
-										<Icon
-											icon="solar:hamburger-menu-linear"
-											width={22}
-											className="text-default-900"
-										/>
-									</Button>
-								)}
-							</DropdownTrigger>
-							<DropdownMenu variant="flat" className="dark" items={menuLinks}>
-								{(menuLinks) => (
-									<DropdownItem
-										key={menuLinks.key}
-										startContent={menuLinks.Icon}
-										href={menuLinks.href}
-									>
-										{menuLinks.label}
-									</DropdownItem>
-								)}
-							</DropdownMenu>
-						</Dropdown>
+						<HeaderUser />
 					</NavbarItem>
 				</NavbarContent>
 			</Navbar>
