e74a3079a3e9b31b22d68a6134fef3190e2645fe
soyricardodev <soyricardodev@proton.me>
2024-05-20 12:25:31 -0400

    feat: add icons to categories and improve categories section

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -1,40 +1,68 @@
-import { Button, ButtonGroup } from "@nextui-org/react";
+import { Button } from "@nextui-org/react";
 import { ChevronRightIcon, MoveUpRightIcon } from "lucide-react";
 import Link from "next/link";
 import AnimatedGradientText from "~/components/magicui/animated-gradient-text";
 import { SearchForm } from "./search-form";
+import {
+	TreePalmIcon,
+	HeartIcon,
+	HandHeartIcon,
+	VideoIcon,
+	RabbitIcon,
+	CircleUserRoundIcon,
+} from "lucide-react";
+
+interface Category {
+	id: number;
+	name: string;
+	href: string;
+	icon: JSX.Element;
+	color: "default" | "primary" | "danger" | "success" | "secondary" | "warning";
+}
 
 export function Hero() {
-	const categories = [
+	const categories: Array<Category> = [
 		{
 			id: 1,
 			name: "Trending",
 			href: "/c/trending",
+			icon: <TreePalmIcon className="size-5" />,
+			color: "default",
 		},
 		{
 			id: 2,
 			name: "Relationship",
 			href: "/c/relationship",
+			icon: <HeartIcon className="size-5" />,
+			color: "secondary",
 		},
 		{
 			id: 3,
 			name: "Dating",
 			href: "/c/dating",
-		},
-		{
-			id: 4,
-			name: "Sex",
-			href: "/c/sex",
-		},
-		{
-			id: 5,
-			name: "Self Care",
-			href: "/c/selfcare",
+			icon: <HandHeartIcon className="size-5" />,
+			color: "primary",
 		},
 		{
 			id: 6,
 			name: "Entertainment",
 			href: "/c/entertainment",
+			icon: <VideoIcon className="size-5" />,
+			color: "warning",
+		},
+		{
+			id: 4,
+			name: "Sex",
+			href: "/c/sex",
+			icon: <RabbitIcon className="size-5" />,
+			color: "danger",
+		},
+		{
+			id: 5,
+			name: "Self Care",
+			href: "/c/selfcare",
+			icon: <CircleUserRoundIcon className="size-5" />,
+			color: "success",
 		},
 	];
 
@@ -70,29 +98,25 @@ export function Hero() {
 					<SearchForm />
 				</div>
 
-				<div className="absolute top-full mx-auto mt-6 flex max-w-full flex-wrap items-center justify-center gap-2 whitespace-nowrap px-4 text-sm">
-					<ButtonGroup size="sm" variant="bordered">
-						{categories.map((categorie, idx) => (
-							<CategoryCTA
-								key={`${categorie.name}-${idx}`}
-								name={categorie.name}
-								href={categorie.href}
-							/>
-						))}
-					</ButtonGroup>
+				<div className="absolute top-full mx-auto mt-6 flex flex-wrap items-center justify-center gap-2 whitespace-nowrap px-4 text-sm max-w-sm md:max-w-xl lg:max-w-2xl">
+					{categories.map((categorie, idx) => (
+						<CategoryCTA key={`${categorie.name}-${idx}`} {...categorie} />
+					))}
 				</div>
 			</div>
 		</div>
 	);
 }
 
-function CategoryCTA({ name, href }: { name: string; href: string }) {
+function CategoryCTA({ name, href, icon, color }: Category) {
 	return (
 		<Button
 			as={Link}
 			href={href}
 			endContent={<MoveUpRightIcon width={15} height={15} />}
-			variant="bordered"
+			startContent={icon}
+			variant="flat"
+			color={color}
 		>
 			{name}
 		</Button>
