9f1530a463e65e42f329587e24de544ef606eb56
soyricardodev <soyricardodev@proton.me>
2024-05-20 09:28:27 -0400

    fix: :bug: add type="button" to button

--- src/app/(user)/[username]/profile-data.tsx
+++ src/app/(user)/[username]/profile-data.tsx
@@ -57,6 +57,7 @@ export function ProfileData({
 				) : null}
 			</div>
 			<button
+				type="button"
 				className="inline-flex shrink-0 items-center justify-center whitespace-nowrap text-sm transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-background font-medium shadow-sm hover:bg-accent hover:text-accent-foreground h-8 py-2 absolute right-0 top-0 w-8 rounded-full px-0 md:static md:w-auto md:rounded-lg md:px-3"
 				onClick={() => setIsEditing(true)}
 			>
