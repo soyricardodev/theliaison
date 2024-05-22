448ba962fcccbe02425bfbff6e754aa55b439200
soyricardodev <soyricardodev@proton.me>
2024-05-22 11:44:20 -0400

    feat: improve ui and removing things

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,20 +1,26 @@
-import Link from "next/link";
+import { Link } from "@nextui-org/react";
 import { LoginForm } from "./_components/login-form";
 
 export default function Signin() {
 	return (
-		<div className="flex w-full max-w-sm flex-col gap-4 rounded-large bg-[hsl(240_5.88%_10%)] px-8 pb-10 pt-6 shadow-sm mx-auto md:mx-0">
-			<p className="pb-2 text-xl font-medium text-white">Login</p>
-			<LoginForm />
-			<p className="text-center text-small text-white">
-				Need to create an account?{" "}
-				<Link
-					className="relative inline-flex items-center tap-highlight-transparent outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 text-small no-underline hover:opacity-80 active:opacity-disabled transition-opacity text-white"
-					href="/signup"
-				>
-					Sign Up
-				</Link>
-			</p>
+		<div className="flex w-full items-center justify-center bg-background lg:w-1/2">
+			<div className="flex w-full max-w-sm flex-col items-center gap-4 p-4">
+				<div className="w-full text-left">
+					<p className="pb-2 text-xl font-medium">Welcome Back</p>
+					<p className="text-small text-default-500">
+						Log in to your account to continue
+					</p>
+				</div>
+
+				<LoginForm />
+
+				<p className="text-center text-small">
+					Need to create an account?&nbsp;
+					<Link href="/signup" size="sm">
+						Sign Up
+					</Link>
+				</p>
+			</div>
 		</div>
 	);
 }
