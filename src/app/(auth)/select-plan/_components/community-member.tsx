66d118361d7beaa03682efa5467cc3e1abd1ce6b
soyricardodev <soyricardodev@proton.me>
2024-05-22 14:56:06 -0400

    feat: move select plan to his own place

---
+++ src/app/(auth)/select-plan/_components/community-member.tsx
@@ -0,0 +1,61 @@
+import { Button } from "@nextui-org/react";
+import { usePathname, useRouter } from "next/navigation";
+import { getErrorRedirect } from "~/utils/helpers";
+import { getStripe } from "~/utils/stripe/client";
+import { checkoutWithStripe } from "~/utils/stripe/server";
+import { createClient } from "~/utils/supabase/client";
+
+export function CommunityMember({ priceId }: { priceId: string }) {
+	const router = useRouter();
+	const currentPath = usePathname();
+
+	const handleStripeCheckout = async () => {
+		const supabase = createClient();
+
+		const {
+			data: { user },
+			error,
+		} = await supabase.auth.getUser();
+
+		if (!user || error) {
+			return router.push("/signup");
+		}
+
+		const { data: price, error: priceError } = await supabase
+			.from("prices")
+			.select("*")
+			.eq("id", priceId)
+			.single();
+
+		if (priceError || !price) {
+			return router.push("/signup");
+		}
+
+		const { errorRedirect, sessionId } = await checkoutWithStripe(
+			price,
+			currentPath,
+		);
+
+		if (errorRedirect) {
+			return router.push(errorRedirect);
+		}
+
+		if (!sessionId) {
+			return router.push(
+				getErrorRedirect(
+					currentPath,
+					"An unknown error occurred.",
+					"Please try again later or contact a system administrator.",
+				),
+			);
+		}
+
+		const stripe = await getStripe();
+		stripe?.redirectToCheckout({ sessionId });
+	};
+	return (
+		<Button color="secondary" className="mx-2" onClick={handleStripeCheckout}>
+			Continue With Community Member
+		</Button>
+	);
+}
