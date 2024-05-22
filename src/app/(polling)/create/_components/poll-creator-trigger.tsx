9c47e6a93e5278114ad418cb070ec9d670116c6a
soyricardodev <soyricardodev@proton.me>
2024-05-22 19:14:39 -0400

    feat: improve light ui

--- src/app/(polling)/create/_components/poll-creator-trigger.tsx
+++ src/app/(polling)/create/_components/poll-creator-trigger.tsx
@@ -22,15 +22,15 @@ export function PollCreatorTrigger() {
 		<>
 			<div className="grid gap-2 max-w-screen-xl px-20">
 				<div
-					className="cursor-pointer transition-colors group bg-content1 hover:bg-content2 px-6 py-8 rounded-md flex justify-between items-center text-white"
+					className="cursor-pointer transition-colors group bg-content4 hover:bg-content3 px-6 py-8 rounded-md flex justify-between items-center"
 					onClick={() => handleOpenModal("scratch")}
 					onKeyDown={() => handleOpenModal("scratch")}
 					tabIndex={0}
 					role="button"
 				>
 					<div className="flex items-center gap-x-4">
-						<div className="p-2 border-2 rounded-full border-yellow-400 group-hover:bg-yellow-400 transition-colors">
-							<ZapIcon className="size-8 text-yellow-400  group-hover:text-black transition-colors" />
+						<div className="p-2 border-2 rounded-full border-yellow-500 group-hover:bg-yellow-500 transition-colors">
+							<ZapIcon className="size-8 text-yellow-500  group-hover:text-black transition-colors" />
 						</div>
 						<div>
 							<h3 className="text-2xl font-bold">Create from scratch</h3>
@@ -38,19 +38,19 @@ export function PollCreatorTrigger() {
 						</div>
 					</div>
 
-					<div className="border-2 border-transparent group-hover:border-white rounded-full p-2 transition-colors">
+					<div className="border-2 border-transparent group-hover:border-black rounded-full p-2 transition-colors">
 						<ArrowRightIcon className="size-8" />
 					</div>
 				</div>
 
 				<div
-					className="cursor-pointer hover:bg-content2 transition-colors group bg-content1 px-6 py-8 rounded-md flex justify-between items-center text-white"
+					className="cursor-pointer hover:bg-content3 transition-colors group bg-content4 px-6 py-8 rounded-md flex justify-between items-center"
 					onClick={() => handleOpenModal("scratch")}
 					onKeyDown={() => handleOpenModal("scratch")}
 				>
 					<div className="flex items-center gap-x-4">
-						<div className="p-2 border-2 rounded-full border-purple-400 group-hover:bg-purple-400 transition-colors">
-							<BotIcon className="size-8 text-purple-400  group-hover:text-black transition-colors" />
+						<div className="p-2 border-2 rounded-full border-purple-500 group-hover:bg-purple-500 transition-colors">
+							<BotIcon className="size-8 text-purple-500  group-hover:text-black transition-colors" />
 						</div>
 						<div>
 							<h3 className="text-2xl font-bold">Create with AI</h3>
@@ -58,17 +58,12 @@ export function PollCreatorTrigger() {
 						</div>
 					</div>
 
-					<div className="border-2 border-transparent group-hover:border-white rounded-full p-2 transition-colors">
+					<div className="border-2 border-transparent group-hover:border-black rounded-full p-2 transition-colors">
 						<ArrowRightIcon className="size-8" />
 					</div>
 				</div>
 			</div>
-			<Modal
-				isOpen={isOpen}
-				className="dark text-white"
-				onOpenChange={onOpenChange}
-				backdrop="blur"
-			>
+			<Modal isOpen={isOpen} onOpenChange={onOpenChange} backdrop="blur">
 				<ModalContent>
 					{(onClose) => (
 						<>
