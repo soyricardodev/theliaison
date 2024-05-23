fce9d7acc5e2cc45d339c6e9e871d0453ac5117e
soyricardodev <soyricardodev@proton.me>
2024-05-23 11:38:19 -0400

    feat: add scrolling animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,12 +15,23 @@ const config = {
 			},
 			animation: {
 				gradient: "gradient 8s linear infinite",
+				"scrolling-banner": "scrolling-banner var(--duration) linear infinite",
+				"scrolling-banner-vertical":
+					"scrolling-banner-vertical var(--duration) linear infinite",
 			},
 			keyframes: {
 				gradient: {
 					to: {
 						backgroundPosition: "var(--bg-size) 0",
 					},
+				},
+				"scrolling-banner": {
+					from: { transform: "translateX(0)" },
+					to: { transform: "translateX(calc(-50% - var(--gap)/2))" },
+				},
+				"scrolling-banner-vertical": {
+					from: { transform: "translateY(0)" },
+					to: { transform: "translateY(calc(-50% - var(--gap)/2))" },
 				},
 			},
 		},
