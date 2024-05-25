c3fb0b02dd90e2eea820967517326638783a64a0
soyricardodev <soyricardodev@proton.me>
2024-05-24 21:23:54 -0400

    feat: add better poll creation

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -1,9 +1,10 @@
+import { randomUUID } from "node:crypto";
 import type { PollFormValues } from "~/app/(polling)/create/_components/create-poll-scratch";
 import { createClient } from "~/utils/supabase/client";
 
 export async function createPollWithOptions(
 	values: PollFormValues,
-	image?: string | null,
+	image: string,
 ) {
 	const supabase = createClient();
 
@@ -16,38 +17,52 @@ export async function createPollWithOptions(
 		throw new Error("Unauthorized");
 	}
 
-	const { data: poll, error: createPollError } = await supabase
-		.from("polls")
-		.insert({
-			question: values.question,
-			user_id: user.id,
-			image,
-		})
-		.select("*")
-		.single();
+	const pollQuestionFormatted = values.question
+		.replace(/[^a-zA-Z0-9]/g, "-")
+		.toLowerCase();
+	const pollRandomId = randomUUID().substring(0, 6);
+	const pollQuestionId = `${pollQuestionFormatted}-${pollRandomId}`;
 
-	if (createPollError || !poll) {
+	const { error: createPollError } = await supabase.from("polls").insert({
+		id: pollQuestionId,
+		question: values.question,
+		user_id: user.id,
+		image,
+	});
+
+	if (createPollError) {
 		throw new Error("Unable to create poll");
 	}
 
-	const optionsToInsert = values.options.map((option) => ({
-		text: option.value,
-		poll_id: poll.id,
-	}));
+	const optionsToInsert = [];
 
-	const { data: pollOptions, error: createPollOptionsError } = await supabase
+	for (const option of values.options) {
+		const optionToInsert = {
+			text: option.value,
+			poll_id: pollQuestionId,
+		};
+
+		optionsToInsert.push(optionToInsert);
+	}
+
+	const { error: createPollOptionsError } = await supabase
 		.from("options")
-		.insert(optionsToInsert)
-		.select();
+		.insert(optionsToInsert);
 
-	if (createPollOptionsError || !pollOptions) {
+	if (createPollOptionsError) {
 		throw new Error("Unable to create poll options");
 	}
 
-	const categoriesToInsert = values.categories.map((category) => ({
-		categorie_id: Number(category),
-		poll_id: poll.id,
-	}));
+	const categoriesToInsert = [];
+
+	for (const category of values.categories) {
+		const categoryToInsert = {
+			categorie_id: Number(category),
+			poll_id: pollQuestionId,
+		};
+
+		categoriesToInsert.push(categoryToInsert);
+	}
 
 	const { error: createPollCategoriesError } = await supabase
 		.from("polls_categories")
@@ -57,7 +72,7 @@ export async function createPollWithOptions(
 		throw new Error("Unable to create poll categories");
 	}
 
-	return poll;
+	return pollQuestionId;
 }
 
 export async function getUserPolls() {
@@ -87,7 +102,7 @@ export async function getUserPolls() {
 	return polls;
 }
 
-export async function insertPollVote(pollId: number, optionId: number) {
+export async function insertPollVote(pollId: string, optionId: number) {
 	const supabase = createClient();
 
 	const {
