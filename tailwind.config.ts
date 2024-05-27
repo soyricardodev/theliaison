e57b16c88f9890e727acd007284bc61f83c7c0a9
soyricardodev <soyricardodev@proton.me>
2024-05-27 16:53:39 -0400

    feat: add tremor theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,44 @@
 import type { Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 import { nextui } from "@nextui-org/react";
+import colors from "tailwindcss/colors";
 
 const config = {
 	darkMode: ["class"],
 	content: [
 		"./src/**/*.{ts,tsx}",
 		"./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+		"./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
 		extend: {
+			colors: {
+				tremor: {
+					brand: {
+						faint: colors.blue[50],
+						muted: colors.blue[200],
+						subtle: colors.blue[400],
+						DEFAULT: colors.blue[500],
+						emphasis: colors.blue[700],
+						inverted: colors.white,
+					},
+					background: {
+						muted: colors.gray[50],
+						subtle: colors.gray[100],
+						DEFAULT: colors.white,
+						emphasis: colors.gray[700],
+					},
+					border: { DEFAULT: colors.gray[200] },
+					ring: { DEFAULT: colors.gray[200] },
+					content: {
+						subtle: colors.gray[400],
+						DEFAULT: colors.gray[500],
+						emphasis: colors.gray[700],
+						strong: colors.gray[900],
+						inverted: colors.white,
+					},
+				},
+			},
 			fontFamily: {
 				sans: ["var(--font-geist-sans)", ...fontFamily.sans],
 			},
