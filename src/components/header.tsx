02405586792a4b52f2b24e55cce07ceabd55a8dd
soyricardodev <soyricardodev@proton.me>
2024-05-28 11:57:26 -0400

    feat: remove unused things and better ui

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,74 +1,31 @@
-import {
-	BreadcrumbItem,
-	Breadcrumbs,
-	Link,
-	Navbar,
-	NavbarBrand,
-	NavbarContent,
-	NavbarItem,
-} from "@nextui-org/react";
 import React from "react";
-
-import { Logo } from "./logo";
 
 import { HeaderCTA } from "./header-cta";
 import { HeaderUser } from "./header-user";
+import { MainNav } from "./main-nav";
+import { MobileNav } from "./mobile-nav";
 
 interface HeaderProps {
-	breadcrumbs?: Array<{
-		name: string;
-		href: string;
-	}>;
 	showCta?: boolean;
 	ctaText?: string | JSX.Element;
 	ctaHref?: string;
 }
 
-export function Header({
-	breadcrumbs,
-	showCta = true,
-	ctaText,
-	ctaHref,
-}: HeaderProps) {
+export function Header({ showCta = true }: HeaderProps) {
 	return (
-		<div className="w-full sticky top-0 z-50">
-			<Navbar
-				position="sticky"
-				classNames={{
-					item: "data-[active=true]:text-primary",
-					wrapper: "px-4 sm:px-6",
-				}}
-				height="64px"
-			>
-				<NavbarBrand>
-					<Link href="/">
-						<Logo className="size-10" />
-					</Link>
-				</NavbarBrand>
-				{/* Breadcrumbs */}
-				{breadcrumbs != null && (
-					<Breadcrumbs className="hidden sm:flex" radius="full">
-						{breadcrumbs.map((breadcrumb) => (
-							<BreadcrumbItem key={breadcrumb.name} href={breadcrumb.href}>
-								{breadcrumb.name}
-							</BreadcrumbItem>
-						))}
-					</Breadcrumbs>
-				)}
+		<header className="w-full sticky top-0 z-50 mx-auto bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+			<div className="container flex h-14 max-w-screen-2xl items-center">
+				<MainNav />
+				<MobileNav />
 
-				{/* Right Menu */}
-				<NavbarContent
-					className="ml-auto h-12 max-w-fit items-center gap-0"
-					justify="end"
-				>
+				<div className="ml-auto h-12 max-w-fit items-center gap-0 flex self-end">
 					{showCta && <HeaderCTA />}
-					{/* User Menu */}
-					<NavbarItem className="px-2">
+					<div className="px-2">
 						<HeaderUser />
-					</NavbarItem>
-				</NavbarContent>
-			</Navbar>
+					</div>
+				</div>
+			</div>
 			<hr className="m-0 h-px w-full border-none bg-gradient-to-r from-neutral-200/0 via-neutral-400/40 to-neutral-200/0" />
-		</div>
+		</header>
 	);
 }
