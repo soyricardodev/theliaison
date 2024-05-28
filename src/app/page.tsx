9b95a9f3aac438cfed9c8accb1afbe1a6aef6f8c
soyricardodev <soyricardodev@proton.me>
2024-05-28 13:41:15 -0400

    remove ppr experimental

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,6 @@ import { Hero } from "./_components/hero";
 import { MainExplore } from "./_components/main-explore";
 import TopBanner from "./_components/top-banner";
 
-export const experimental_ppr = true;
-
 export default function HomePage() {
 	return (
 		<>
