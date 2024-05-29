c1cab66f638793e90b368676b19c34bff1429bbc
soyricardodev <soyricardodev@proton.me>
2024-05-29 19:56:26 -0400

    feat: add better code

--- src/app/(polling)/explore/page.tsx
+++ src/app/(polling)/explore/page.tsx
@@ -1,12 +1,31 @@
 import { Container } from "~/components/container";
-import { createClient } from "~/utils/supabase/server";
+import { Polls } from "./_components/polls";
+import type { PollWithOptionsAndVotes } from "~/types/poll";
+import { getURL } from "~/utils/helpers";
+
+const fetchPolls = async () => {
+	const url = getURL("/api/polls/get");
+	console.log(url);
+	const res = await fetch(url, {
+		method: "GET",
+		next: { tags: ["polls"] },
+	});
+
+	const data = await res.json();
+
+	return data.data as PollWithOptionsAndVotes[];
+};
 
 export default async function ExplorePage() {
-	const supabase = createClient();
+	const data = await fetchPolls();
+
+	console.log({ data });
 
 	return (
 		<Container>
-			<h1>Explore</h1>
+			<h2 className="text-4xl font-semibold">Explore</h2>
+
+			<Polls polls={data} />
 		</Container>
 	);
 }
