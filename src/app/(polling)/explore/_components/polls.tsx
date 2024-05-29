7cb166291094a5cd12a39948cb5775ddebfd120e
soyricardodev <soyricardodev@proton.me>
2024-05-29 19:57:10 -0400

    feat: add polls component

---
+++ src/app/(polling)/explore/_components/polls.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useDebounceCallback } from "usehooks-ts";
+import { NewPollsAproach } from "~/app/_components/polls";
+import { useMounted } from "~/hooks/use-mounted";
+import type { PollWithOptionsAndVotes } from "~/types/poll";
+
+export function Polls({ polls }: { polls: Array<PollWithOptionsAndVotes> }) {
+	const [loadedPolls, setLoadedPolls] =
+		useState<Array<PollWithOptionsAndVotes>>(polls);
+	const mounted = useMounted();
+
+	function handleScroll() {
+		console.log("scrolled debounced");
+	}
+	const handleDebouncedScroll = useDebounceCallback(handleScroll, 200);
+
+	useEffect(() => {
+		if (!mounted) return;
+		window.addEventListener("scroll", handleDebouncedScroll);
+		return () => {
+			window.removeEventListener("scroll", handleDebouncedScroll);
+		};
+	}, [mounted, handleDebouncedScroll]);
+
+	return (
+		<div className="grid gap-4">
+			<NewPollsAproach polls={loadedPolls} />
+		</div>
+	);
+}
