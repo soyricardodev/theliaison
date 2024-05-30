f9b33a244538e3250d468625dfaa09e41c9aec3d
soyricardodev <soyricardodev@proton.me>
2024-05-29 22:46:08 -0400

    style: remove console.log

--- src/app/api/polls/create/route.ts
+++ src/app/api/polls/create/route.ts
@@ -32,17 +32,9 @@ export async function POST(req: Request) {
 	}
 
 	const json = await req.json();
-	console.log({ json });
-	const { data, success, error: parseError } = pollFormSchema.safeParse(json);
+	const { data, success } = pollFormSchema.safeParse(json);
 
 	if (!success) {
-		console.log({
-			success,
-			parseError,
-			data,
-			detailederror: parseError.errors,
-			issues: parseError.issues,
-		});
 		return new Response("Invalid request", {
 			status: 400,
 		});
@@ -57,8 +49,6 @@ export async function POST(req: Request) {
 
 	const embedding = embeddingResponse.data[0]?.embedding;
 
-	console.log(embedding);
-
 	const { data: poll, error: createPollError } = await supabase
 		.from("polls")
 		// @ts-expect-error - TODO: Fix this, embedding is not a string
@@ -71,7 +61,6 @@ export async function POST(req: Request) {
 		});
 
 	if (createPollError) {
-		console.log(createPollError);
 		return new Response("Error creating poll", {
 			status: 500,
 		});
