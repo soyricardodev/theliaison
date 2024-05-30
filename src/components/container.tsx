cfec95de59098302bd422fe2f753f4c43fe91126
soyricardodev <soyricardodev@proton.me>
2024-05-30 11:32:30 -0400

    feat: add className prop

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,7 +1,14 @@
-export function Container({ children }: React.HTMLAttributes<HTMLDivElement>) {
+import { cn } from "~/lib/utils";
+
+export function Container({
+	children,
+	className,
+}: React.HTMLAttributes<HTMLDivElement>) {
 	return (
 		<section className="max-w-screen-2xl px-6 lg:px-8 mt-16 mx-auto mb-28">
-			<div className="mx-auto max-w-2xl lg:max-w-none">{children}</div>
+			<div className={cn("mx-auto max-w-2xl lg:max-w-none", className)}>
+				{children}
+			</div>
 		</section>
 	);
 }
