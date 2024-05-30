e33e3f984045bd913e8fcc1e32470e61a4533236
soyricardodev <soyricardodev@proton.me>
2024-05-30 14:12:35 -0400

    feat: add queryclient provider

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,11 +1,17 @@
-// app/providers.tsx
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
+
+const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
 	const router = useRouter();
 
-	return <NextUIProvider navigate={router.push}>{children}</NextUIProvider>;
+	return (
+		<QueryClientProvider client={queryClient}>
+			<NextUIProvider navigate={router.push}>{children}</NextUIProvider>
+		</QueryClientProvider>
+	);
 }
