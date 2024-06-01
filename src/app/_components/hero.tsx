44068b287bd3df316ae91ba22d353971bafdb5f0
soyricardodev <soyricardodev@proton.me>
2024-05-31 22:25:37 -0400

    feat: remove brackets and add font-heading

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -13,17 +13,13 @@ export function Hero() {
 					<Link href="/ai" className="z-10 flex items-center justify-center">
 						<AnimatedGradientText>
 							🎉 <hr className="mx-2 h-4 w-[1px] shrink-0 bg-gray-400/80" />{" "}
-							<span
-								className={
-									"inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent"
-								}
-							>
+							<span className="inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent">
 								Introducing The Liaison AI
 							</span>
 							<ChevronRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
 						</AnimatedGradientText>
 					</Link>
-					<h1 className="bg-gradient-to-br bg-clip-text py-6 text-5xl font-semibold leading-none tracking-tighter text-transparent from-black from-40% to-black/40 sm:text-6xl md:text-7xl lg:text-7xl text-balance">
+					<h1 className="bg-gradient-to-br bg-clip-text py-6 text-5xl font-semibold leading-none tracking-tighter text-transparent from-black from-40% to-black/40 sm:text-6xl md:text-7xl lg:text-7xl text-balance font-heading">
 						Ask The Liaison
 					</h1>
 					<p className="max-w-[60rem] text-pretty text-lg tracking-tight text-gray-500 md:text-xl">
