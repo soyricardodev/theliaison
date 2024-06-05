ee4135202912bfb655b862b03610c6f2d09b7c81
soyricardodev <soyricardodev@proton.me>
2024-06-05 19:53:14 -0400

    feat:add base gift page

--- src/app/gift/page.tsx
+++ src/app/gift/page.tsx
@@ -1,13 +1,17 @@
 import { FAQs } from "./_components/faqs";
 import { Hero } from "./_components/hero";
+import { HeroAnimation } from "./_components/hero-animation";
 import { Testimonials } from "./_components/testimonials";
 
 export default function GiftingConciergePage() {
 	return (
-		<main className="flex-1 overflow-auto max-w-screen-2xl mx-auto flex flex-col">
-			<Hero />
-			<Testimonials />
-			<FAQs />
-		</main>
+		<>
+			<HeroAnimation />
+			<main className="flex-1 overflow-auto max-w-screen-2xl mx-auto flex flex-col">
+				<Hero />
+				<Testimonials />
+				<FAQs />
+			</main>
+		</>
 	);
 }
