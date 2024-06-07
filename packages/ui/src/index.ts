5be337e1ed8f63dc9cb9a4e810628c6d10adfdce
soyricardodev <soyricardodev@proton.me>
2024-06-07 16:16:59 -0400

    feat: add shadcn components

---
+++ packages/ui/src/index.ts
@@ -0,0 +1,24 @@
+import { cx } from "class-variance-authority";
+import { extendTailwindMerge } from "tailwind-merge";
+
+const COMMON_UNITS = ["small", "medium", "large"];
+
+const twMerge = extendTailwindMerge({
+  extend: {
+    theme: {
+      opacity: ["disabled"],
+      spacing: ["divider"],
+      borderWidth: COMMON_UNITS,
+      borderRadius: COMMON_UNITS,
+    },
+    classGroups: {
+      shadow: [{ shadow: COMMON_UNITS }],
+      "font-size": [{ text: ["tiny", ...COMMON_UNITS] }],
+      "bg-image": ["bg-stripe-gradient"],
+    },
+  },
+});
+
+export function cn(...inputs: Parameters<typeof cx>) {
+  return twMerge(cx(inputs));
+}
