2d304b789e8b4430191ab861529a82604c5d7996
soyricardodev <soyricardodev@proton.me>
2024-06-09 08:42:26 -0400

    feat: update github url and api version

--- apps/social-polling/src/utils/stripe/config.ts
+++ apps/social-polling/src/utils/stripe/config.ts
@@ -5,13 +5,12 @@ import { env } from "~/env";
 export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
   // https://github.com/stripe/stripe-node#configuration
   // https://stripe.com/docs/api/versioning
-  // @ts-ignore
-  apiVersion: null,
+  apiVersion: "2024-04-10",
   // Register this as an official Stripe plugin.
   // https://stripe.com/docs/building-plugins#setappinfo
   appInfo: {
     name: "The Liaison",
     version: "0.0.0",
-    url: "https://github.com/soyricardodev/polling-liaison",
+    url: "https://github.com/soyricardodev/theliaison",
   },
 });
