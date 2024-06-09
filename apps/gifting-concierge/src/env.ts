d0a03ce634935efe69cfdbe24cb6e45c8c72fadb
soyricardodev <soyricardodev@proton.me>
2024-06-09 08:45:48 -0400

    feat: initial setup

---
+++ apps/gifting-concierge/src/env.ts
@@ -0,0 +1,30 @@
+/* eslint-disable no-restricted-properties */
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string().url(),
+    STRIPE_SECRET_KEY: z.string(),
+    STRIPE_WEBHOOK_SECRET: z.string(),
+    SUPABASE_SERVICE_ROLE_KEY: z.string(),
+    OPENAI_API_KEY: z.string(),
+  },
+  client: {
+    NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string(),
+    NEXT_PUBLIC_PROJECT_ID: z.string(),
+  },
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+
+  emptyStringAsUndefined: true,
+
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_PROJECT_ID: process.env.NEXT_PUBLIC_PROJECT_ID,
+  },
+});
---
+++ apps/gifting-concierge/src/supabase/image-loader.js
@@ -0,0 +1,16 @@
+import { env } from "~/env";
+
+const projectId = env.NEXT_PUBLIC_PROJECT_ID;
+
+/**
+ * @param {Object} param0
+ * @param {string} param0.src
+ * @param {number} param0.width
+ * @param {number} param0.quality
+ *
+ */
+export default function supabaseLoader({ src, width, quality }) {
+  return `https://${projectId}.supabase.co/storage/v1/object/public/${src}?width=${width}&quality=${
+    quality || 75
+  }`;
+}
