7427f17ea9205c2c23ca2f74d86fff619b3ad2c3
soyricardodev <soyricardodev@proton.me>
2024-06-09 21:22:15 -0400

    [BUG]: plugins dont loading

--- tooling/prettier/index.js
+++ tooling/prettier/index.js
@@ -6,31 +6,31 @@ import { fileURLToPath } from "url";
 
 /** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
 const config = {
-  plugins: [
-    "@ianvs/prettier-plugin-sort-imports",
-    "prettier-plugin-tailwindcss",
-  ],
-  tailwindConfig: fileURLToPath(
-    new URL("../../tooling/tailwind/web.ts", import.meta.url),
-  ),
-  tailwindFunctions: ["cn", "cva"],
-  importOrder: [
-    "<TYPES>",
-    "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
-    "^(next/(.*)$)|^(next$)",
-    "^(expo(.*)$)|^(expo$)",
-    "<THIRD_PARTY_MODULES>",
-    "",
-    "<TYPES>^@theliaison",
-    "^@theliaison/(.*)$",
-    "",
-    "<TYPES>^[.|..|~]",
-    "^~/",
-    "^[../]",
-    "^[./]",
-  ],
-  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
-  importOrderTypeScriptVersion: "4.4.0",
+  // plugins: [
+  //   // "@ianvs/prettier-plugin-sort-imports",
+  //   "prettier-plugin-tailwindcss",
+  // ],
+  // tailwindConfig: fileURLToPath(
+  //   new URL("../../tooling/tailwind/web.ts", import.meta.url),
+  // ),
+  // tailwindFunctions: ["cn", "cva"],
+  // importOrder: [
+  //   "<TYPES>",
+  //   "^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
+  //   "^(next/(.*)$)|^(next$)",
+  //   "^(expo(.*)$)|^(expo$)",
+  //   "<THIRD_PARTY_MODULES>",
+  //   "",
+  //   "<TYPES>^@theliaison",
+  //   "^@theliaison/(.*)$",
+  //   "",
+  //   "<TYPES>^[.|..|~]",
+  //   "^~/",
+  //   "^[../]",
+  //   "^[./]",
+  // ],
+  // importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  // importOrderTypeScriptVersion: "4.4.0",
 };
 
 export default config;
