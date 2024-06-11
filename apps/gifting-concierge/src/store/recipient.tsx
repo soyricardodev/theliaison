daceaf77ec8118a9dead0f072ae16a7aae1fe2c5
soyricardodev <soyricardodev@proton.me>
2024-06-10 23:20:40 -0400

    feat: add recipientstore

---
+++ apps/gifting-concierge/src/store/recipient.tsx
@@ -0,0 +1,35 @@
+import { create } from "zustand";
+
+export interface Recipient {
+  canContinue: boolean;
+  setCanContinue: (canContinue: boolean) => void;
+
+  recipientName: string;
+  setRecipientName: (recipientName: string) => void;
+
+  recipientSocial: string;
+  setRecipientSocial: (recipientSocial: string) => void;
+
+  recipientEmail?: string;
+  setRecipientEmail: (recipientEmail: string) => void;
+
+  recipientPhone?: string;
+  setRecipientPhone: (recipientPhone: string) => void;
+}
+
+export const useRecipientStore = create<Recipient>((set) => ({
+  canContinue: false,
+  setCanContinue: (canContinue) => set({ canContinue }),
+
+  recipientName: "",
+  setRecipientName: (recipientName) => set({ recipientName }),
+
+  recipientSocial: "",
+  setRecipientSocial: (recipientSocial) => set({ recipientSocial }),
+
+  recipientEmail: undefined,
+  setRecipientEmail: (recipientEmail) => set({ recipientEmail }),
+
+  recipientPhone: undefined,
+  setRecipientPhone: (recipientPhone) => set({ recipientPhone }),
+}));
