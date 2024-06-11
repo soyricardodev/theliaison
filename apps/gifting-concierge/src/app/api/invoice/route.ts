b238f696814c228a41e2547f7b1266291a0da41c
soyricardodev <soyricardodev@proton.me>
2024-06-11 00:36:32 -0400

    feat: add simple create invoice

---
+++ apps/gifting-concierge/src/app/api/invoice/route.ts
@@ -0,0 +1,51 @@
+import { NextResponse } from "next/server";
+import { createClient } from "~/supabase/server";
+import { stripe } from "~/utils/stripe/config";
+import { z } from "zod";
+
+const createInvoice = z.object({
+  email: z.string().email(),
+  products: z.array(
+    z.object({
+      product_id: z.string(),
+      price_id: z.string(),
+      quantity: z.number(),
+    }),
+  ),
+});
+
+export async function POST(request: Request) {
+  const json = (await request.json()) as Record<string, unknown>;
+  const parsed = createInvoice.safeParse(json);
+
+  if (!parsed.success) {
+    return NextResponse.json(parsed.error.format());
+  }
+
+  const { email, products } = parsed.data;
+
+  const newCustomer = await stripe.customers.create({
+    email,
+    description: "Customer to invoice the gifting concierge",
+  });
+  const customerId = newCustomer.id;
+
+  const invoice = await stripe.invoices.create({
+    customer: customerId,
+    collection_method: "send_invoice",
+    days_until_due: 14,
+  });
+
+  for await (const product of products) {
+    await stripe.invoiceItems.create({
+      customer: customerId,
+      price: product.price_id,
+      quantity: product.quantity,
+      invoice: invoice.id,
+    });
+  }
+
+  const invoiceSend = await stripe.invoices.sendInvoice(invoice.id);
+
+  return NextResponse.json({ invoiceSend });
+}
