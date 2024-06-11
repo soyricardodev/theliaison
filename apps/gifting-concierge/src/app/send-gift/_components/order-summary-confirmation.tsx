82c7aab03b3e829da29736915728bcf4e4f0f215
soyricardodev <soyricardodev@proton.me>
2024-06-10 23:21:57 -0400

    feat: add order summary

---
+++ apps/gifting-concierge/src/app/send-gift/_components/order-summary-confirmation.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { useCartStore } from "~/store/cart";
+import OrderSummaryItem from "./order-summary-item";
+import { useRecipientStore } from "~/store/recipient";
+import {
+  UserRoundIcon,
+  InstagramIcon,
+  MailIcon,
+  PhoneIcon,
+} from "lucide-react";
+
+export function OrderSummaryConfirmation() {
+  const { shoppingCart } = useCartStore();
+  const { recipientName, recipientSocial, recipientEmail, recipientPhone } =
+    useRecipientStore();
+
+  console.log({
+    recipientName,
+    recipientSocial,
+    recipientEmail,
+    recipientPhone,
+  });
+
+  return (
+    <div className="flex flex-col gap-6 mb-2">
+      <h4 className="font-medium text-default-500">Confirm gift details</h4>
+      <ul>
+        {shoppingCart.map((item) => (
+          <OrderSummaryItem key={item.id} {...item} />
+        ))}
+      </ul>
+
+      <h4 className="font-medium text-default-500">
+        Confirm recipient details
+      </h4>
+
+      <ul className="flex flex-col gap-2 [&>li]:text-lg [&>li>strong]:flex [&>li>strong]:gap-2 [&>li>strong]:items-center [&>li>strong]:font-semibold">
+        <li>
+          <strong>
+            <UserRoundIcon />
+            Name:
+          </strong>{" "}
+          {recipientName}
+        </li>
+        <li>
+          <strong>
+            <InstagramIcon /> Social:
+          </strong>{" "}
+          {recipientSocial}
+        </li>
+        <li>
+          <strong>
+            <MailIcon /> Email:
+          </strong>{" "}
+          {recipientEmail?.trim() === "" || recipientEmail == null
+            ? "- No email provided"
+            : recipientEmail}
+        </li>
+        <li>
+          <strong>
+            <PhoneIcon /> Phone:
+          </strong>{" "}
+          {recipientPhone?.trim() === "" || recipientPhone == null
+            ? "- No phone provided"
+            : recipientPhone}
+        </li>
+      </ul>
+    </div>
+  );
+}
