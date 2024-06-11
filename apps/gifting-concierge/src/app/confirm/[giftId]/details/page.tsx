008a988618a6829d503b78ee36b1ca495fdd7e8e
soyricardodev <soyricardodev@proton.me>
2024-06-10 23:24:35 -0400

    feat: add recipient details form page

---
+++ apps/gifting-concierge/src/app/confirm/[giftId]/details/page.tsx
@@ -0,0 +1,9 @@
+import ShippingForm from "./shipping-form";
+
+export default function Page({
+  params: { giftId },
+}: {
+  params: { giftId: string };
+}) {
+  return <ShippingForm giftId={giftId} />;
+}
