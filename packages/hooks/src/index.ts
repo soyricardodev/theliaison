0b11c689107191a876d67d64087fe5d5b3db7abf
soyricardodev <soyricardodev@proton.me>
2024-06-12 22:43:01 -0400

    feat: add hooks

---
+++ packages/hooks/src/index.ts
@@ -0,0 +1,2 @@
+export * from "./use-media-query";
+export * from "./use-isomorphic-layout-effect";
---
+++ packages/hooks/src/use-isomorphic-layout-effect.ts
@@ -0,0 +1,15 @@
+import { useEffect, useLayoutEffect } from "react";
+
+/**
+ * Custom hook that uses either `useLayoutEffect` or `useEffect` based on the environment (client-side or server-side).
+ * @param {Function} effect - The effect function to be executed.
+ * @param {Array<any>} [dependencies] - An array of dependencies for the effect (optional).
+ * @example
+ * ```tsx
+ * useIsomorphicLayoutEffect(() => {
+ *   // Code to be executed during the layout phase on the client side
+ * }, [dependency1, dependency2]);
+ * ```
+ */
+export const useIsomorphicLayoutEffect =
+	typeof window !== "undefined" ? useLayoutEffect : useEffect;
