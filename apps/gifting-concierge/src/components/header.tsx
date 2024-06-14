c806c7919da3900a1f038a1817b0142ecdd3a554
soyricardodev <soyricardodev@proton.me>
2024-06-14 13:55:30 -0400

    feat: add created components

--- apps/gifting-concierge/src/components/header.tsx
+++ apps/gifting-concierge/src/components/header.tsx
@@ -1,13 +1,6 @@
-"use client";
-
 import {
-	Avatar,
 	Badge,
 	Button,
-	Dropdown,
-	DropdownItem,
-	DropdownMenu,
-	DropdownTrigger,
 	Input,
 	Link,
 	Navbar,
@@ -21,18 +14,13 @@ import {
 	PopoverContent,
 	PopoverTrigger,
 } from "@nextui-org/react";
-import { BellIcon, SearchIcon, ShoppingCartIcon } from "lucide-react";
+import { BellIcon, SearchIcon } from "lucide-react";
 import React from "react";
-import { useCartStore } from "~/store/cart";
 import { NotificationsCard } from "./notifications/notifications-header-card";
+import { UserMenu } from "./header/user-menu";
+import { ShoppingCartCta } from "./header/shopping-cart-cta";
 
 export function Header() {
-	const { shoppingCart, setIsOpen, isOpen } = useCartStore();
-
-	const handleToggleCart = () => {
-		setIsOpen(!isOpen);
-	};
-
 	return (
 		<Navbar
 			isBordered
@@ -76,22 +64,7 @@ export function Header() {
 				</NavbarItem>
 				{/* Shopping Cart */}
 				<NavbarItem>
-					<Button
-						isIconOnly
-						radius="full"
-						variant="light"
-						className="overflow-visible"
-						onClick={handleToggleCart}
-					>
-						<Badge
-							color="success"
-							content={shoppingCart.length}
-							showOutline={false}
-							size="md"
-						>
-							<ShoppingCartIcon className="text-default-500 size-6" />
-						</Badge>
-					</Button>
+					<ShoppingCartCta />
 				</NavbarItem>
 				{/* Notifications */}
 				<NavbarItem className="flex">
@@ -116,40 +89,7 @@ export function Header() {
 				</NavbarItem>
 				{/* User Menu */}
 				<NavbarItem className="px-2">
-					<Dropdown placement="bottom-end">
-						<DropdownTrigger>
-							<button className="mt-1 h-8 w-8 transition-transform">
-								<Badge
-									color="success"
-									content=""
-									placement="bottom-right"
-									shape="circle"
-								>
-									<Avatar
-										size="sm"
-										src="https://i.pravatar.cc/150?u=a04258114e29526708c"
-									/>
-								</Badge>
-							</button>
-						</DropdownTrigger>
-						<DropdownMenu aria-label="Profile Actions" variant="flat">
-							<DropdownItem key="profile" className="h-14 gap-2">
-								<p className="font-semibold">Signed in as</p>
-								<p className="font-semibold">johndoe@example.com</p>
-							</DropdownItem>
-							<DropdownItem key="settings">My Settings</DropdownItem>
-							<DropdownItem key="team_settings">Team Settings</DropdownItem>
-							<DropdownItem key="analytics">Analytics</DropdownItem>
-							<DropdownItem key="system">System</DropdownItem>
-							<DropdownItem key="configurations">Configurations</DropdownItem>
-							<DropdownItem key="help_and_feedback">
-								Help & Feedback
-							</DropdownItem>
-							<DropdownItem key="logout" color="danger">
-								Log Out
-							</DropdownItem>
-						</DropdownMenu>
-					</Dropdown>
+					<UserMenu />
 				</NavbarItem>
 			</NavbarContent>
 
