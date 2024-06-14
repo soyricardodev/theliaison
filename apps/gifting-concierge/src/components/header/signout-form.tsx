a9681ff502ceeaad174ed1d5189861fd8e0ed144
soyricardodev <soyricardodev@proton.me>
2024-06-14 15:28:17 -0400

    feat: add ui and icon

--- apps/gifting-concierge/src/components/header/signout-form.tsx
+++ apps/gifting-concierge/src/components/header/signout-form.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import { signOut } from "./actions";
+import { LogOutIcon } from "lucide-react";
 
 export function SignOutForm() {
 	return (
-		<form action={signOut}>
-			<button type="submit">Log Out</button>
+		<form
+			action={signOut}
+			className="relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 w-full"
+		>
+			<button type="submit" className="flex gap-2 w-full">
+				<LogOutIcon className="size-5" />
+				Log Out
+			</button>
 		</form>
 	);
 }
