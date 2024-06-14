5ce70f1ccabb86cc327816e2a1fe255ea2844988
soyricardodev <soyricardodev@proton.me>
2024-06-14 17:21:20 -0400

    feat: remove unused set and remove because we have a middleware refreshing user sessions

--- apps/gifting-concierge/src/supabase/server.ts
+++ apps/gifting-concierge/src/supabase/server.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-import type { CookieOptions } from "@supabase/ssr";
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
@@ -17,24 +15,6 @@ export function createClient() {
 			cookies: {
 				get(name: string) {
 					return cookieStore.get(name)?.value;
-				},
-				set(name: string, value: string, options: CookieOptions) {
-					try {
-						cookieStore.set({ name, value, ...options });
-					} catch (error) {
-						// The `set` method was called from a Server Component.
-						// This can be ignored if you have middleware refreshing
-						// user sessions.
-					}
-				},
-				remove(name: string, options: CookieOptions) {
-					try {
-						cookieStore.set({ name, value: "", ...options });
-					} catch (error) {
-						// The `delete` method was called from a Server Component.
-						// This can be ignored if you have middleware refreshing
-						// user sessions.
-					}
 				},
 			},
 		},
