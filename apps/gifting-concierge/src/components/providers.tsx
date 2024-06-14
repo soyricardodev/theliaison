a15638e11d3451ecef7959983f5c2d38b91c2bea
soyricardodev <soyricardodev@proton.me>
2024-06-14 13:55:10 -0400

    feat: add sonner toaster from ui

--- apps/gifting-concierge/src/components/providers.tsx
+++ apps/gifting-concierge/src/components/providers.tsx
@@ -2,12 +2,14 @@
 
 import { NextUIProvider } from "@nextui-org/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@theliaison/ui/sonner";
 
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
 	return (
 		<NextUIProvider>
+			<Toaster />
 			<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 		</NextUIProvider>
 	);
