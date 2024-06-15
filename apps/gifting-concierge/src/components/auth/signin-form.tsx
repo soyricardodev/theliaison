696164e6482fd856f8636007ad93a58025e46b1a
soyricardodev <soyricardodev@proton.me>
2024-06-15 00:30:31 -0400

    feat: add brand

--- apps/gifting-concierge/src/components/auth/signin-form.tsx
+++ apps/gifting-concierge/src/components/auth/signin-form.tsx
@@ -42,14 +42,7 @@ export default function SignIn() {
 		<div className="w-full sm:w-[26rem] shadow sm:p-5  border dark:border-zinc-800 rounded-md">
 			<div className="p-5 space-y-5">
 				<div className="text-center space-y-3">
-					<Image
-						src={"/supabase.png"}
-						alt="supabase logo"
-						width={50}
-						height={50}
-						className=" rounded-full mx-auto"
-					/>
-					<h1 className="font-bold">Sign in to SupaAuth</h1>
+					<h1 className="font-bold">Sign in to The Liaison</h1>
 					<p className="text-sm">Welcome back! Please sign in to continue</p>
 				</div>
 				<SocialAuth redirectTo={next || "/"} />
