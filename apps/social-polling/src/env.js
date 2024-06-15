89a9e0b6240bf57cff5a2b9e6973ddce52e51d87
soyricardodev <soyricardodev@proton.me>
2024-06-15 15:46:48 -0400

    feat: add gc url and remove comments

--- apps/social-polling/src/env.js
+++ apps/social-polling/src/env.js
@@ -2,10 +2,6 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-	/**
-	 * Specify your server-side environment variables schema here. This way you can ensure the app
-	 * isn't built with invalid env vars.
-	 */
 	server: {
 		NODE_ENV: z.enum(["development", "test", "production"]),
 		DATABASE_URL: z.string().url(),
@@ -13,36 +9,21 @@ export const env = createEnv({
 		STRIPE_WEBHOOK_SECRET: z.string(),
 		SUPABASE_SERVICE_ROLE_KEY: z.string(),
 		OPENAI_API_KEY: z.string(),
+		GIFTING_CONCIERGE_URL: z.string().url(),
 	},
 
-	/**
-	 * Specify your client-side environment variables schema here. This way you can ensure the app
-	 * isn't built with invalid env vars. To expose them to the client, prefix them with
-	 * `NEXT_PUBLIC_`.
-	 */
 	client: {
-		// NEXT_PUBLIC_CLIENTVAR: z.string(),
 		NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
 		NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
 		NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string(),
 		NEXT_PUBLIC_PROJECT_ID: z.string(),
 	},
 
-	/**
-	 * You can't destruct `process.env` as a regular object in the Next.js edge runtimes (e.g.
-	 * middlewares) or client-side so we need to destruct manually.
-	 */
-	runtimeEnv: {
-		NODE_ENV: process.env.NODE_ENV,
+	experimental__runtimeEnv: {
 		NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 		NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-		DATABASE_URL: process.env.DATABASE_URL,
-		STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
 		NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
 			process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
-		STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
-		SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
-		OPENAI_API_KEY: process.env.OPENAI_API_KEY,
 		NEXT_PUBLIC_PROJECT_ID: process.env.NEXT_PUBLIC_PROJECT_ID,
 	},
 	/**
@@ -50,9 +31,6 @@ export const env = createEnv({
 	 * useful for Docker builds.
 	 */
 	skipValidation: true,
-	/**
-	 * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
-	 * `SOME_VAR=''` will throw an error.
-	 */
+
 	emptyStringAsUndefined: true,
 });
