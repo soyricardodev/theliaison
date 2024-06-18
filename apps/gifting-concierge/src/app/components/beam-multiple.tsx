d64b378e92c33a24c070d64cb8309893d8f344ab
soyricardodev <soyricardodev@proton.me>
2024-06-18 11:15:47 -0400

    fix: import cn from ui

--- apps/gifting-concierge/src/app/components/beam-multiple.tsx
+++ apps/gifting-concierge/src/app/components/beam-multiple.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { cn } from "@/lib/utils";
+import { cn } from "@theliaison/ui";
 import { AnimatedBeam } from "@theliaison/ui/magicui/animated-beam";
 import { forwardRef, useRef } from "react";
 import type React from "react";
@@ -123,6 +123,7 @@ const Icons = {
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
 		>
+			<title>Notion</title>
 			<path
 				d="M6.017 4.313l55.333 -4.087c6.797 -0.583 8.543 -0.19 12.817 2.917l17.663 12.443c2.913 2.14 3.883 2.723 3.883 5.053v68.243c0 4.277 -1.553 6.807 -6.99 7.193L24.467 99.967c-4.08 0.193 -6.023 -0.39 -8.16 -3.113L3.3 79.94c-2.333 -3.113 -3.3 -5.443 -3.3 -8.167V11.113c0 -3.497 1.553 -6.413 6.017 -6.8z"
 				fill="#ffffff"
@@ -142,6 +143,8 @@ const Icons = {
 			viewBox="0 0 24 24"
 			xmlns="http://www.w3.org/2000/svg"
 		>
+			<title>OpenAI</title>
+
 			<path d="M22.2819 9.8211a5.9847 5.9847 0 0 0-.5157-4.9108 6.0462 6.0462 0 0 0-6.5098-2.9A6.0651 6.0651 0 0 0 4.9807 4.1818a5.9847 5.9847 0 0 0-3.9977 2.9 6.0462 6.0462 0 0 0 .7427 7.0966 5.98 5.98 0 0 0 .511 4.9107 6.051 6.051 0 0 0 6.5146 2.9001A5.9847 5.9847 0 0 0 13.2599 24a6.0557 6.0557 0 0 0 5.7718-4.2058 5.9894 5.9894 0 0 0 3.9977-2.9001 6.0557 6.0557 0 0 0-.7475-7.0729zm-9.022 12.6081a4.4755 4.4755 0 0 1-2.8764-1.0408l.1419-.0804 4.7783-2.7582a.7948.7948 0 0 0 .3927-.6813v-6.7369l2.02 1.1686a.071.071 0 0 1 .038.052v5.5826a4.504 4.504 0 0 1-4.4945 4.4944zm-9.6607-4.1254a4.4708 4.4708 0 0 1-.5346-3.0137l.142.0852 4.783 2.7582a.7712.7712 0 0 0 .7806 0l5.8428-3.3685v2.3324a.0804.0804 0 0 1-.0332.0615L9.74 19.9502a4.4992 4.4992 0 0 1-6.1408-1.6464zM2.3408 7.8956a4.485 4.485 0 0 1 2.3655-1.9728V11.6a.7664.7664 0 0 0 .3879.6765l5.8144 3.3543-2.0201 1.1685a.0757.0757 0 0 1-.071 0l-4.8303-2.7865A4.504 4.504 0 0 1 2.3408 7.872zm16.5963 3.8558L13.1038 8.364 15.1192 7.2a.0757.0757 0 0 1 .071 0l4.8303 2.7913a4.4944 4.4944 0 0 1-.6765 8.1042v-5.6772a.79.79 0 0 0-.407-.667zm2.0107-3.0231l-.142-.0852-4.7735-2.7818a.7759.7759 0 0 0-.7854 0L9.409 9.2297V6.8974a.0662.0662 0 0 1 .0284-.0615l4.8303-2.7866a4.4992 4.4992 0 0 1 6.6802 4.66zM8.3065 12.863l-2.02-1.1638a.0804.0804 0 0 1-.038-.0567V6.0742a4.4992 4.4992 0 0 1 7.3757-3.4537l-.142.0805L8.704 5.459a.7948.7948 0 0 0-.3927.6813zm1.0976-2.3654l2.602-1.4998 2.6069 1.4998v2.9994l-2.5974 1.4997-2.6067-1.4997Z" />
 		</svg>
 	),
@@ -152,6 +155,8 @@ const Icons = {
 			viewBox="0 0 87.3 78"
 			xmlns="http://www.w3.org/2000/svg"
 		>
+			<title>Google Drive</title>
+
 			<path
 				d="m6.6 66.85 3.85 6.65c.8 1.4 1.95 2.5 3.3 3.3l13.75-23.8h-27.5c0 1.55.4 3.1 1.2 4.5z"
 				fill="#0066da"
@@ -185,6 +190,7 @@ const Icons = {
 			viewBox="0 0 175.216 175.552"
 			xmlns="http://www.w3.org/2000/svg"
 		>
+			<title>WhatsApp</title>
 			<defs>
 				<linearGradient
 					id="b"
@@ -239,6 +245,7 @@ const Icons = {
 			viewBox="0 0 47 65"
 			xmlns="http://www.w3.org/2000/svg"
 		>
+			<title>Google Docs</title>
 			<defs>
 				<path
 					d="M29.375,0 L4.40625,0 C1.9828125,0 0,1.99431818 0,4.43181818 L0,60.5681818 C0,63.0056818 1.9828125,65 4.40625,65 L42.59375,65 C45.0171875,65 47,63.0056818 47,60.5681818 L47,17.7272727 L29.375,0 Z"
@@ -327,7 +334,7 @@ const Icons = {
 											fillRule="nonzero"
 											mask="url(#mask-4)"
 											points="30.6638281 16.4309659 47 32.8582386 47 17.7272727"
-										></polygon>
+										/>
 									</g>
 									<g id="Clipped">
 										<mask id="mask-7" fill="white">
@@ -422,6 +429,7 @@ const Icons = {
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
 		>
+			<title>Zapier</title>
 			<path
 				d="M57.1877 45.2253L57.1534 45.1166L78.809 25.2914V15.7391H44.0663V25.2914H64.8181L64.8524 25.3829L43.4084 45.2253V54.7775H79.1579V45.2253H57.1877Z"
 				fill="#201515"
@@ -460,6 +468,7 @@ const Icons = {
 			viewBox="0 0 48 48"
 			xmlns="http://www.w3.org/2000/svg"
 		>
+			<title>Messenger</title>
 			<radialGradient
 				id="8O3wK6b5ASW2Wn6hRCB5xa_YFbzdUk7Q3F8_gr1"
 				cx="11.087"
@@ -468,13 +477,13 @@ const Icons = {
 				gradientTransform="matrix(1 0 0 -1 0 50)"
 				gradientUnits="userSpaceOnUse"
 			>
-				<stop offset="0" stopColor="#1292ff"></stop>
-				<stop offset=".079" stopColor="#2982ff"></stop>
-				<stop offset=".23" stopColor="#4e69ff"></stop>
-				<stop offset=".351" stopColor="#6559ff"></stop>
-				<stop offset=".428" stopColor="#6d53ff"></stop>
-				<stop offset=".754" stopColor="#df47aa"></stop>
-				<stop offset=".946" stopColor="#ff6257"></stop>
+				<stop offset="0" stopColor="#1292ff" />
+				<stop offset=".079" stopColor="#2982ff" />
+				<stop offset=".23" stopColor="#4e69ff" />
+				<stop offset=".351" stopColor="#6559ff" />
+				<stop offset=".428" stopColor="#6d53ff" />
+				<stop offset=".754" stopColor="#df47aa" />
+				<stop offset=".946" stopColor="#ff6257" />
 			</radialGradient>
 			<path
 				fill="url(#8O3wK6b5ASW2Wn6hRCB5xa_YFbzdUk7Q3F8_gr1)"
@@ -504,6 +513,7 @@ const Icons = {
 			strokeWidth="2"
 			xmlns="http://www.w3.org/2000/svg"
 		>
+			<title>User</title>
 			<path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
 			<circle cx="12" cy="7" r="4" />
 		</svg>
