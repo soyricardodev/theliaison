266507e449536f41f62407d84b919dd735a2b384
soyricardodev <soyricardodev@proton.me>
2024-06-19 11:48:05 -0400

    feat: add text-balance and replace teams with brands

--- apps/gifting-concierge/src/app/components/companies.tsx
+++ apps/gifting-concierge/src/app/components/companies.tsx
@@ -8,8 +8,8 @@ export function Companies() {
 		>
 			<div className="py-14">
 				<div className="mx-auto max-w-screen-xl px-4 md:px-8">
-					<h3 className="text-center text-sm font-semibold text-gray-600">
-						TRUSTED BY TEAMS FROM AROUND THE WORLD
+					<h3 className="text-center text-balance text-sm font-semibold text-gray-600">
+						TRUSTED BY BRANDS FROM AROUND THE WORLD
 					</h3>
 					<div className="relative mt-6">
 						<ul className="flex flex-wrap items-center justify-center gap-x-10 gap-y-6 md:gap-x-16 [&_path]:fill-black ">
