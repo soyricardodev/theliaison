02b84251b5ebeb79dbd0331e179e0806a2c21f91
soyricardodev <soyricardodev@proton.me>
2024-06-19 18:44:17 -0400

    feat: add reverse prop and improve styles

--- apps/gifting-concierge/src/app/components/bento/gifts-marquee.tsx
+++ apps/gifting-concierge/src/app/components/bento/gifts-marquee.tsx
@@ -3,7 +3,10 @@ import Marquee from "@theliaison/ui/magicui/marquee";
 import Image from "next/image";
 import { createClient } from "~/supabase/server";
 
-export async function GiftsMarquee() {
+export async function GiftsMarquee({
+	className,
+	reverse,
+}: { className?: string; reverse?: boolean }) {
 	const supabase = createClient();
 	const { data, error } = await supabase
 		.from("products")
@@ -17,13 +20,17 @@ export async function GiftsMarquee() {
 	return (
 		<Marquee
 			pauseOnHover
-			className="absolute top-10 [--duration:20s] [mask-image:linear-gradient(to_top,transparent_40%,#000_100%)] "
+			className={cn(
+				"absolute top-10 [--duration:20s] [mask-image:linear-gradient(to_top,transparent_10%,#000_100%)]",
+				className,
+			)}
+			reverse={reverse}
 		>
 			{data?.map((product, idx) => (
 				<figure
 					key={`${idx}-${product.name}-${product.id}`}
 					className={cn(
-						"relative w-32 cursor-pointer overflow-hidden rounded-xl border p-4",
+						"relative w-44 cursor-pointer overflow-hidden rounded-xl border p-4",
 						"border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
 						"transform-gpu blur-[1px] transition-all duration-300 ease-out hover:blur-none",
 					)}
@@ -35,7 +42,7 @@ export async function GiftsMarquee() {
 								alt={product.name ?? ""}
 								width={80}
 								height={80}
-								className="h-auto w-32"
+								className="h-auto w-full"
 							/>
 							<figcaption className="text-sm font-medium">
 								{product.name}
