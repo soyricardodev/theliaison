ae39efaf1da0a190a0c7b67574ee6207220f2037
soyricardodev <soyricardodev@proton.me>
2024-06-19 13:02:07 -0400

    feat: small update of bento

--- apps/gifting-concierge/src/app/components/bento.tsx
+++ apps/gifting-concierge/src/app/components/bento.tsx
@@ -11,8 +11,9 @@ import {
 } from "@theliaison/ui/command";
 import { BentoCard, BentoGrid } from "@theliaison/ui/magicui/bento-grid";
 import Marquee from "@theliaison/ui/magicui/marquee";
-import { Share2Icon } from "lucide-react";
+import { GiftIcon, Share2Icon } from "lucide-react";
 import { AnimatedBeamMultipleOutputDemo } from "./beam-multiple";
+import { GiftsMarquee } from "./bento/gifts-marquee";
 
 const files = [
 	{
@@ -118,39 +119,13 @@ const Notification = ({ name, description, icon, color, time }: Item) => {
 
 const features = [
 	{
-		Icon: FileTextIcon,
-		name: "Save your files",
+		Icon: GiftIcon,
+		name: "Find your perfect gift",
 		description: "We automatically save your files as you type.",
 		href: "/",
 		cta: "Learn more",
 		className: "col-span-3 lg:col-span-1",
-		background: (
-			<Marquee
-				pauseOnHover
-				className="absolute top-10 [--duration:20s] [mask-image:linear-gradient(to_top,transparent_40%,#000_100%)] "
-			>
-				{files.map((f, idx) => (
-					<figure
-						key={`${idx}-${f.name}`}
-						className={cn(
-							"relative w-32 cursor-pointer overflow-hidden rounded-xl border p-4",
-							"border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
-							"dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
-							"transform-gpu blur-[1px] transition-all duration-300 ease-out hover:blur-none",
-						)}
-					>
-						<div className="flex flex-row items-center gap-2">
-							<div className="flex flex-col">
-								<figcaption className="text-sm font-medium dark:text-white ">
-									{f.name}
-								</figcaption>
-							</div>
-						</div>
-						<blockquote className="mt-2 text-xs">{f.body}</blockquote>
-					</figure>
-				))}
-			</Marquee>
-		),
+		background: <GiftsMarquee />,
 	},
 	{
 		Icon: InputIcon,
