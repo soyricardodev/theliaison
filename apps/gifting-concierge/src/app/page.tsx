c6c793dec5fc9c8d5f4baaabfd1199d11a25f79d
soyricardodev <soyricardodev@proton.me>
2024-06-19 13:58:40 -0400

    feat: add features

--- apps/gifting-concierge/src/app/page.tsx
+++ apps/gifting-concierge/src/app/page.tsx
@@ -2,6 +2,7 @@ import { BentoDemo } from "./components/bento";
 import { CallToAction } from "./components/call-to-action";
 import { Companies } from "./components/companies";
 import { FAQs } from "./components/faq";
+import { Features } from "./components/features";
 import { Footer } from "./components/footer";
 import { Hero } from "./components/hero";
 import { SocialProofTestimonials } from "./components/testimonials";
@@ -13,7 +14,7 @@ export default function Home() {
 				<Hero />
 				<Companies />
 				<div className="[--color:var(--color-one)] pointer-events-none relative z-10 mx-auto h-[50rem] overflow-hidden [mask-image:radial-gradient(ellipse_at_center_center,#000,transparent_50%)] my-[-18.8rem] before:absolute before:inset-0 before:h-full before:w-full before:opacity-40 before:[background-image:radial-gradient(circle_at_bottom_center,var(--color),transparent_70%)] after:absolute after:-left-1/2 after:top-1/2 after:aspect-[1/0.7] after:w-[200%] after:rounded-[50%] after:border-t after:border-[hsl(var(--border))] after:bg-background after:z-50 before:z-50" />
-				<section id="something">
+				<section id="how-it-works">
 					<div className="mx-auto flex max-w-screen-xl flex-col gap-8 px-4 py-14 md:px-8">
 						<div className="mx-auto max-w-5xl text-center">
 							<h4 className="text-xl font-bold tracking-tight text-black">
@@ -30,6 +31,7 @@ export default function Home() {
 							</p>
 						</div>
 						<BentoDemo />
+						<Features />
 					</div>
 				</section>
 				<SocialProofTestimonials />
