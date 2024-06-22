b8d30af42b4bef02b24b3da0844803ac3d93f64c
soyricardodev <soyricardodev@proton.me>
2024-06-21 21:44:58 -0400

    feat: proper creation of gift

--- apps/gifting-concierge/src/app/send-gift/actions.tsx
+++ apps/gifting-concierge/src/app/send-gift/actions.tsx
@@ -1,39 +1,72 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 "use server";
 
+import { redirect } from "next/navigation";
 import { createClient } from "~/supabase/server";
 
 interface InsertGift {
-	total_price: number;
-	sender_name?: string;
 	recipient_name: string;
-	recipient_social: string;
+	recipient_social?: string;
+	recipient_social_handle?: string;
 	recipient_email?: string;
 	recipient_phone?: string;
 	gifts: {
 		id: string;
 		quantity: number;
 	}[];
+	knows_address: boolean;
 }
 
 export async function createGift({
-	total_price,
-	sender_name,
 	recipient_name,
 	recipient_social,
+	recipient_social_handle,
 	recipient_email,
+	recipient_phone,
 	gifts,
+	knows_address,
 }: InsertGift) {
 	const supabase = createClient();
 
-	const { data, error } = await supabase
+	const {
+		data: { user },
+		error: authError,
+	} = await supabase.auth.getUser();
+
+	if (authError || !user) {
+		redirect("/login");
+	}
+
+	const { data: createGiftRecipientQuery, error: recipientError } =
+		await supabase
+			.from("gift_recipients")
+			.insert({
+				name: recipient_name,
+				knows_address,
+				provided_contact: true,
+			})
+			.select("id")
+			.single();
+
+	if (recipientError || !createGiftRecipientQuery) {
+		console.log({ recipientError });
+		return recipientError;
+	}
+
+	await supabase.from("gift_recipient_contacts").insert({
+		recipient_id: createGiftRecipientQuery.id,
+		email: recipient_email,
+		phone_number: recipient_phone,
+		social_media_handle: recipient_social,
+		social_media_platform: recipient_social_handle,
+	});
+
+	const { data: createGiftOrderQuery, error } = await supabase
 		.from("gifts")
 		.insert({
-			total_price,
-			sender_name,
-			recipient_name,
-			recipient_social,
-			recipient_email,
+			sender_id: user.id,
+			recipient_id: createGiftRecipientQuery.id,
+			status: "awaiting_recipient_confirmation",
 		})
 		.select("id")
 		.single();
@@ -44,12 +77,11 @@ export async function createGift({
 
 	for await (const gift of gifts) {
 		await supabase.from("gifts_products").insert({
-			gift_id: data.id,
+			gift_id: createGiftOrderQuery.id,
 			product_id: gift.id,
 			quantity: gift.quantity,
 		});
 	}
 
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-return
-	return data.id;
+	return createGiftOrderQuery.id;
 }
