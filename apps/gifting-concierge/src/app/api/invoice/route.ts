cdc9dc57d24e6370ba7076e88c63cca46a262293
soyricardodev <soyricardodev@proton.me>
2024-06-24 21:48:10 -0400

    feat: send invoice link with resend

--- apps/gifting-concierge/src/app/api/invoice/route.ts
+++ apps/gifting-concierge/src/app/api/invoice/route.ts
@@ -1,85 +1,30 @@
-import { NextResponse } from "next/server";
-import { z } from "zod";
-import { createClient } from "~/supabase/server";
-import { stripe } from "~/utils/stripe/config";
+import { EmailTemplate } from "./email-template";
+import { Resend } from "resend";
+import type React from "react";
+import { type NextRequest, NextResponse } from "next/server";
 
-const createInvoice = z.object({
-	giftId: z.string(),
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(request: Request) {
-	const json = (await request.json()) as Record<string, unknown>;
-	const parsed = createInvoice.safeParse(json);
+export async function POST(request: NextRequest) {
+	try {
+		const { email, invoiceLink } = (await request.json()) as {
+			email: string;
+			invoiceLink: string;
+		};
 
-	if (!parsed.success) {
-		return NextResponse.json(parsed.error.format());
-	}
-
-	const { giftId } = parsed.data;
-
-	const email = "ricardo@mail.com";
-
-	const supabase = createClient();
-
-	const { data, error } = await supabase
-		.from("gifts")
-		.select("gifts_products(quantity, products(prices(id)))")
-		.eq("id", giftId)
-		.single();
-
-	if (error) {
-		return NextResponse.json(error);
-	}
-
-	const products: {
-		price_id: string;
-		quantity: number;
-	}[] = [];
-
-	for (const giftItem of data.gifts_products) {
-		if (giftItem.products == null) continue;
-
-		const priceId = giftItem.products?.prices[0]?.id;
-
-		if (priceId == null) continue;
-
-		products.push({
-			price_id: priceId,
-			quantity: giftItem.quantity,
+		const { data, error } = await resend.emails.send({
+			from: "The Liaison <hello@theliaison.com>",
+			to: [email],
+			subject: "Your Gifting Concierge Invoice",
+			react: EmailTemplate({ invoiceLink }) as React.ReactElement,
 		});
+
+		if (error) {
+			return NextResponse.json({ error }, { status: 500 });
+		}
+
+		return NextResponse.json({ data });
+	} catch (error) {
+		return NextResponse.json({ error }, { status: 500 });
 	}
-
-	const newCustomer = await stripe.customers.create({
-		email,
-		description: "Customer to invoice the gifting concierge",
-	});
-	const customerId = newCustomer.id;
-
-	const invoice = await stripe.invoices.create({
-		customer: customerId,
-		collection_method: "send_invoice",
-		days_until_due: 14,
-	});
-
-	for await (const product of products) {
-		await stripe.invoiceItems.create({
-			customer: customerId,
-			price: product.price_id,
-			quantity: product.quantity,
-			invoice: invoice.id,
-		});
-	}
-
-	const invoiceSend = await stripe.invoices.sendInvoice(invoice.id);
-
-	const invoiceHostedLink = invoiceSend.hosted_invoice_url;
-
-	await supabase
-		.from("gift_payments")
-		.update({
-			invoice_link: invoiceHostedLink,
-		})
-		.eq("id", giftId);
-
-	return NextResponse.json({ invoiceSend });
 }
