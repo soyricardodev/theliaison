12c6db990819a228427eb144a8441b9c5c1b9d95
Ricardo Castro <soyricardodev@proton.me>
2024-06-30 20:21:06 +0200

    feat: reduce logo size and remove get started button in favor of menu toggle

--- apps/gifting-concierge/src/app/confirm/[giftId]/header.tsx
+++ apps/gifting-concierge/src/app/confirm/[giftId]/header.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { InstagramIcon } from "lucide-react";
+import { InstagramIcon, MenuIcon } from "lucide-react";
 
 export function Header() {
 	return (
@@ -9,34 +9,17 @@ export function Header() {
 				<Image
 					src="/logo-white.webp"
 					alt="The Liaison Logo"
-					width={45}
-					height={45}
+					width={35}
+					height={35}
 				/>
-				<p className="text-white hidden sm:inline-flex">
+				<p className="text-white hidden sm:inline-flex text-sm gap-1">
 					TL <strong>Gifting Concierge</strong>
 				</p>
 			</Link>
 
-			<ul className="flex items-center justify-center gap-2">
-				<li className="mr-4 hidden sm:inline-flex">
-					<a
-						href="https://instagram.com"
-						target="_blank"
-						className="flex items-center justify-center"
-						rel="noreferrer"
-					>
-						<InstagramIcon className="size-6 text-white hover:text-white/90 transition-colors" />
-					</a>
-				</li>
-				<li>
-					<Link
-						href="/register"
-						className="text-sm text-black rounded-md hover:bg-white/80 transition-colors py-2 px-3 bg-white"
-					>
-						Get Started
-					</Link>
-				</li>
-			</ul>
+			<button type="button">
+				<MenuIcon className="text-white hover:text-white/90 transition-colors size-5" />
+			</button>
 		</header>
 	);
 }
