0b223527c6084ef148da0fcfc8ffb3804b74ca0c
Ricardo Castro <soyricardodev@proton.me>
2024-06-30 16:22:48 +0200

    feat: add options for send a gift with drawer

--- apps/gifting-concierge/src/app/components/hero.tsx
+++ apps/gifting-concierge/src/app/components/hero.tsx
@@ -1,14 +1,22 @@
 "use client";
 
 import { cn } from "@theliaison/ui";
-import { buttonVariants } from "@theliaison/ui/button";
-import { BorderBeam } from "@theliaison/ui/magicui/border-beam";
+import { Button, buttonVariants } from "@theliaison/ui/button";
 import { Particles } from "@theliaison/ui/magicui/particles";
 import { motion, useInView } from "framer-motion";
-import { GiftIcon, StoreIcon } from "lucide-react";
+import { GiftIcon, StoreIcon, CircleHelpIcon, LinkIcon } from "lucide-react";
 import Link from "next/link";
 import { useRef } from "react";
-import { CircleHelpIcon } from "lucide-react";
+import {
+	Drawer,
+	DrawerClose,
+	DrawerContent,
+	DrawerDescription,
+	DrawerFooter,
+	DrawerHeader,
+	DrawerTitle,
+	DrawerTrigger,
+} from "@theliaison/ui/drawer";
 
 export function Hero() {
 	const fadeInRef = useRef(null);
@@ -77,7 +85,7 @@ export function Hero() {
 			<motion.div
 				animate={fadeInInView ? "animate" : "initial"}
 				variants={fadeUpVariants}
-				className="grid grid-cols-1 gap-4 mx-auto w-full max-w-sm"
+				className="grid grid-cols-1 gap-2 mx-auto w-full max-w-sm"
 				initial={false}
 				transition={{
 					duration: 0.6,
@@ -86,50 +94,90 @@ export function Hero() {
 					type: "spring",
 				}}
 			>
-				<Link
-					href="/giftshop"
-					className={cn(
-						buttonVariants({ size: "lg" }),
-						"flex gap-2 items-center text-base",
-					)}
-				>
-					Choose Gift
-					<GiftIcon className="size-5 translate-x-0 transition-all duration-300 ease-out group-hover:translate-x-1" />
-				</Link>
+				<Drawer>
+					<DrawerTrigger asChild>
+						<Button size="lg" className="flex gap-2 items-center text-base">
+							Choose Gift
+							<GiftIcon className="size-5 translate-x-0 transition-all duration-300 ease-out group-hover:translate-x-1" />
+						</Button>
+					</DrawerTrigger>
+					<DrawerContent className="max-w-[361px] mx-auto rounded-[36px] bg-[#FEFFFE]">
+						<div className="px-6 pb-6 pt-2 5">
+							<DrawerClose asChild>
+								<button
+									className="absolute z-10 flex h-8 w-8 items-center justify-center rounded-full bg-[#F7F8F9] text-[#949595] transition-transform focus:scale-95 focus-visible:shadow-focus-ring-button active:scale-75"
+									type="button"
+									style={{ top: 28, right: 28 }}
+								>
+									<svg
+										width="12"
+										height="12"
+										viewBox="0 0 12 12"
+										fill="none"
+										xmlns="http://www.w3.org/2000/svg"
+										className="vaul-scrollable"
+									>
+										<title>Close</title>
+										<path
+											d="M10.4854 1.99998L2.00007 10.4853"
+											stroke="#999999"
+											strokeWidth="3"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+										/>
+										<path
+											d="M10.4854 10.4844L2.00007 1.99908"
+											stroke="#999999"
+											strokeWidth="3"
+											strokeLinecap="round"
+											strokeLinejoin="round"
+										/>
+									</svg>
+								</button>
+							</DrawerClose>
+							<header className="mb-4 -mt-6 flex h-[72px] items-center border-b border-[#F7F7F7] pl-2">
+								<h2 className="text-[19px] font-semibold text-[#222222] md:font-medium">
+									Options
+								</h2>
+							</header>
+
+							<div className="space-y-3">
+								<a
+									href="/giftshop"
+									className="textLeft flex h-12 w-full items-center gap-[15px] rounded-[16px] bg-[#F7F8F9] px-4 text-[17px] font-semibold text-[#222222] transition-transform focus:scale-95 focus-visible:shadow-focus-ring-button active:scale-95 md:font-medium"
+								>
+									<StoreIcon />
+									Our Gifts Shop
+								</a>
+								<button
+									type="button"
+									className="textLeft flex h-12 w-full items-center gap-[15px] rounded-[16px] bg-[#F7F8F9] px-4 text-[17px] font-semibold text-[#222222] transition-transform focus:scale-95 focus-visible:shadow-focus-ring-button active:scale-95 md:font-medium"
+								>
+									<GiftIcon />
+									Send a custom gift
+								</button>
+								<button
+									type="button"
+									className="textLeft flex h-12 w-full items-center gap-[15px] rounded-[16px] bg-[#F7F8F9] px-4 text-[17px] font-semibold text-[#222222] transition-transform focus:scale-95 focus-visible:shadow-focus-ring-button active:scale-95 md:font-medium"
+								>
+									<LinkIcon />
+									Send a gift from a link
+								</button>
+							</div>
+						</div>
+					</DrawerContent>
+				</Drawer>
 
 				<Link
 					href="/how-it-works"
 					className={cn(
-						buttonVariants({ variant: "secondary", size: "lg" }),
-						"flex gap-2 items-center text-base",
+						buttonVariants({ variant: "ghost", size: "lg" }),
+						"text-base",
 					)}
 				>
 					How it Works
-					<CircleHelpIcon className="size-5 translate-x-0 transition-all duration-300 ease-out group-hover:translate-x-1" />
 				</Link>
 			</motion.div>
-
-			{/* <motion.div
-				animate={fadeInInView ? "animate" : "initial"}
-				variants={fadeUpVariants}
-				initial={false}
-				transition={{
-					duration: 0.6,
-					delay: 0.4,
-					ease: [0.21, 0.47, 0.32, 0.98],
-					type: "spring",
-				}}
-				className="relative mt-[8rem] [perspective:2000px] after:absolute after:inset-0 after:z-50 after:[background:linear-gradient(to_top,hsl(var(--background))_30%,transparent)]"
-			>
-				<div className="rounded-xl border border-white/10 bg-white bg-opacity-[0.01] before:absolute before:bottom-1/2 before:left-0 before:top-0 before:h-full before:w-full before:opacity-0 before:[filter:blur(180px)] before:[background-image:linear-gradient(to_bottom,#ffbd7a,#ffbd7a,transparent_40%)] before:animate-image-glow">
-					<BorderBeam />
-					<img
-						className="block relative w-full h-full  rounded-[inherit] border object-contain"
-						src="/dashboard-light.png"
-						alt="dashboard"
-					/>
-				</div>
-			</motion.div> */}
 		</section>
 	);
 }
