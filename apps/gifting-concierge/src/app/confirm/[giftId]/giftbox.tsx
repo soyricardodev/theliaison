21f5c5d8ed8621a6f96195a6bc579d1beb1b24b4
Ricardo Castro <soyricardodev@proton.me>
2024-07-03 14:59:35 -0400

    feat: add offset based on the mouse position

--- apps/gifting-concierge/src/app/confirm/[giftId]/giftbox.tsx
+++ apps/gifting-concierge/src/app/confirm/[giftId]/giftbox.tsx
@@ -1,37 +1,44 @@
 "use client";
 
-import {
-	Tooltip,
-	TooltipContent,
-	TooltipProvider,
-	TooltipTrigger,
-} from "@theliaison/ui/tooltip";
+import { Tooltip } from "@nextui-org/react";
 import Image from "next/image";
 import giftboxGIF from "~/assets/giftbox2.gif";
 import { useConfirmDialogStore } from "~/store/confirm-dialog";
+import { useState, useRef } from "react";
 
 export function Giftbox() {
 	const { isOpen, setIsOpen } = useConfirmDialogStore();
+	const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+	const [offset, setOffset] = useState(0);
 
-	const toggleDialog = () => {
-		setIsOpen(!isOpen);
+	const imageRef = useRef<HTMLImageElement | null>(null);
+
+	const handleMouseEnter = (e: React.MouseEvent) => {
+		setMousePosition({ x: e.clientX, y: e.clientY });
+
+		if (imageRef.current) {
+			const { top, height } = imageRef.current.getBoundingClientRect();
+			const offsetY = top - mousePosition.y + (height / 4);
+
+			console.log({ offsetY });
+			setOffset(offsetY);
+		}
 	};
 
+	const toggleDialog = () => setIsOpen(!isOpen)
+
 	return (
-		<TooltipProvider>
-			<Tooltip>
-				<TooltipTrigger asChild>
-					<Image
-						src={giftboxGIF}
-						alt="Giftbox"
-						className="mx-auto max-w-md hover:scale-105 transition-all hover:[filter:drop-shadow(0_0_8px_#f0f0f0)] w-72 md:w-96"
-						width={384}
-						height={384}
-						onClick={toggleDialog}
-					/>
-				</TooltipTrigger>
-				<TooltipContent onClick={toggleDialog}>Receive gift</TooltipContent>
-			</Tooltip>
-		</TooltipProvider>
+		<Tooltip content="Receive gift" offset={offset} delay={1500} onClick={toggleDialog}>
+			<Image
+				ref={imageRef}
+				src={giftboxGIF}
+				alt="Giftbox"
+				className="mx-auto max-w-md hover:scale-105 transition-all hover:[filter:drop-shadow(0_0_8px_#f0f0f0)] w-72 md:w-96"
+				width={384}
+				height={384}
+				onClick={toggleDialog}
+				onMouseEnter={handleMouseEnter}
+			/>
+		</Tooltip>
 	);
 }
