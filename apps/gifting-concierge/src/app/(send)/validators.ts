c43294cccd2aa3d502f0791f889d5d1d8e1fbfad
Ricardo Castro <soyricardodev@proton.me>
2024-07-04 19:32:09 -0400

    feat: add send custom gift schema

--- apps/gifting-concierge/src/app/(send)/validators.ts
+++ apps/gifting-concierge/src/app/(send)/validators.ts
@@ -11,3 +11,11 @@ export const sendByLinkSchema = z.object({
 		.string()
 		.min(5, { message: "Gift specifications are required" }),
 });
+
+export const sendCustomSchema = z.object({
+	recipientName: z.string().min(1, { message: "Recipient name is required" }),
+	recipientEmail: z.string().email({ message: "Invalid email" }).optional(),
+	recipientSocial: z.string().optional(),
+	recipientSocialHandle: z.string().optional(),
+	recipientPhone: z.string().optional(),
+});
