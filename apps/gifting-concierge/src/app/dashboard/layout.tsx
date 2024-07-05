72e1d6443f96e16f7170676c8ceb94c7a9528306
Ricardo Castro <soyricardodev@proton.me>
2024-07-05 01:30:38 -0400

    feat: add layout with sidebar

--- apps/gifting-concierge/src/app/dashboard/layout.tsx
+++ apps/gifting-concierge/src/app/dashboard/layout.tsx
@@ -1,9 +1,15 @@
-import { DashboardSidebar } from "~/components/sidebar";
+import { SidebarWrapper } from "~/components/sidebar/sidebar-wrapper";
 
 export default function DashboardLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
-	return <DashboardSidebar>{children}</DashboardSidebar>;
+	return (
+		<div className="flex h-dvh w-full">
+			<SidebarWrapper />
+
+			<div className="w-full flex-1 flex-col p-4">{children}</div>
+		</div>
+	);
 }
