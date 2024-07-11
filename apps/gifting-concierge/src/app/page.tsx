a9b4a06bd3e201d3b78816a7b427ff46ac648301
Ricardo Castro <soyricardodev@proton.me>
2024-07-11 19:43:54 -0400

    feat: add gradient to home page

--- apps/gifting-concierge/src/app/page.tsx
+++ apps/gifting-concierge/src/app/page.tsx
@@ -2,18 +2,20 @@ import { Header } from "~/components/header";
 import { QuotesRotate } from "~/components/quotes-rotate";
 import { Footer } from "./components/footer";
 import { Hero } from "./components/hero";
+import { Gradient } from "~/components/gradient";
 
 export default function Home() {
 	return (
 		<>
 			<Header />
-			<main className="antialiased overflow-x-hidden">
+			<main className="antialiased overflow-x-hidden absolute top-0 left-0 w-full h-full z-30">
 				<div className="max-w-7xl mx-auto px-4 flex min-h-screen flex-col items-center justify-between">
 					<Hero />
 				</div>
+				<QuotesRotate />
+				<Footer />
 			</main>
-			<QuotesRotate />
-			<Footer />
+			<Gradient />
 		</>
 	);
 }
