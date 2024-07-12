00d8aaa373513dd6771aa022544a506f3cd85bd3
Ricardo Castro <soyricardodev@proton.me>
2024-07-12 15:20:05 -0400

    feat: add schemas

--- apps/gifting-concierge/src/components/auth/actions.ts
+++ apps/gifting-concierge/src/components/auth/actions.ts
@@ -10,25 +10,24 @@ import { createServerAction } from "zsa";
 import SupaAuthVerifyEmail from "~/emails";
 import { env } from "~/env";
 import supabaseAdmin from "~/supabase/admin";
+import { simpleSignUpSchema, verifyOtpSchema } from "~/lib/validators/auth";
 
 export const verifyOtp = createServerAction()
-	.input(
-		z.object({
-			email: z.string().email({ message: "Invalid email address" }),
-			otp: z.string().min(6, { message: "Invalid OTP" }),
-		}),
-	)
-	.output(z.string())
+	.input(verifyOtpSchema)
 	.handler(async ({ input }) => {
 		const supabase = createClient();
+		console.log("here is verifyotp");
 
-		const res = await supabase.auth.verifyOtp({
+		const { data, error } = await supabase.auth.verifyOtp({
 			email: input.email,
 			token: input.otp,
 			type: "email",
 		});
 
-		return JSON.stringify(res);
+		return {
+			data,
+			error,
+		};
 	});
 
 export const signInWithEmail = createServerAction()
@@ -59,16 +58,11 @@ export const signInWithEmail = createServerAction()
 	});
 
 export const signUpAction = createServerAction()
-	.input(
-		z.object({
-			email: z.string().email({ message: "Invalid email address" }),
-			password: z.string().min(6, { message: "Invalid password" }),
-		}),
-	)
+	.input(simpleSignUpSchema)
 	.handler(async ({ input }) => {
 		const supabase = supabaseAdmin();
 		const resend = new Resend(env.RESEND_API_KEY);
-
+		console.log("here");
 		const res = await supabase.auth.admin.generateLink({
 			type: "signup",
 			email: input.email,
@@ -77,9 +71,9 @@ export const signUpAction = createServerAction()
 
 		if (res.data.properties?.email_otp) {
 			await resend.emails.send({
-				from: `The Liaison <support@${env.RESEND_DOMAIN}>`,
+				from: `The Liaison <noreply@${env.RESEND_DOMAIN}>`,
 				to: [input.email],
-				subject: "Verify Email",
+				subject: "Verify Email Address for The Liaison",
 				react: SupaAuthVerifyEmail({
 					verificationCode: res.data.properties?.email_otp,
 				}),
@@ -87,4 +81,6 @@ export const signUpAction = createServerAction()
 		}
 
 		if (res.error) throw new Error(res.error.message);
+
+		redirect(`/register/confirm/${input.email}`);
 	});
