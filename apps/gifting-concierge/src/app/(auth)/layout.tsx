4da80a9910d4f03f3d70f01243b28a350849b73a
Ricardo Castro <soyricardodev@proton.me>
2024-07-12 15:20:57 -0400

    feat: add gradient

--- apps/gifting-concierge/src/app/(auth)/layout.tsx
+++ apps/gifting-concierge/src/app/(auth)/layout.tsx
@@ -1,10 +1,18 @@
+import { Toaster } from "@theliaison/ui/sonner";
+import { Gradient } from "~/components/gradient";
 import { Header } from "~/components/header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<div className="min-h-dvh">
-			<Header />
-			{children}
-		</div>
+		<>
+			<div className="min-h-dvh antialiased overflow-x-hidden absolute top-0 left-0 w-full h-full z-30">
+				<Header />
+				{children}
+			</div>
+
+			<Gradient color1="#809bd6" color2="#910aff" color3="#af38ff" />
+
+			<Toaster />
+		</>
 	);
 }
