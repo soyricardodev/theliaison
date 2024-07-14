1245250991b79a8f5681c90b39c6f64b8704c85c
Ricardo Castro <soyricardodev@proton.me>
2024-07-14 18:35:43 -0400

    feat: add black color

--- apps/gifting-concierge/src/app/confirm/[giftId]/page.tsx
+++ apps/gifting-concierge/src/app/confirm/[giftId]/page.tsx
@@ -38,7 +38,7 @@ export default async function Confirm({
 	}
 
 	return (
-		<div className="flex flex-col relative z-20">
+		<div className="flex flex-col relative">
 			<section className="w-full py-12 md:py-24 lg:py-32 h-[calc(100vh-60px)] flex flex-col items-center justify-center">
 				<Giftbox />
 				<div className="relative mx-auto flex max-w-2xl flex-col items-center">
@@ -47,13 +47,13 @@ export default async function Confirm({
 							Your Gifting Concierge
 						</span>
 					</p>
-					<h1 className="text-center bg-[linear-gradient(110deg,#f1f1f1,45%,#DBD0C5,55%,#f1f1f1)] bg-[length:190%_100%] bg-clip-text text-transparent font-medium tracking-tighter text-4xl/none sm:text-5xl/none text-pretty">
+					<h1 className="text-center bg-[linear-gradient(110deg,#111,45%,#DBD0C5,55%,#111)] bg-[length:190%_100%] bg-clip-text text-transparent font-medium tracking-tighter text-4xl/none sm:text-5xl/none text-pretty">
 						Someone wants to{" "}
-						<span className="inline-flex animate-background-shine bg-[linear-gradient(110deg,#939393,45%,#DBD0C5,55%,#939393)] bg-[length:250%_100%] bg-clip-text text-transparent leading-tight">
+						<span className="inline-flex animate-background-shine bg-[linear-gradient(110deg,#111,45%,#DBD0C5,55%,#111)] bg-[length:250%_100%] bg-clip-text text-transparent leading-tight">
 							send you a gift!
 						</span>
 					</h1>
-					<p className="mb-8 mt-4 max-w-[30rem] text-left leading-7 text-base md:text-[1.125rem] md:leading-[1.5] text-default-800 font-normal">
+					<p className="mb-8 mt-4 max-w-[30rem] text-left leading-7 text-base md:text-[1.125rem] md:leading-[1.5] text-black font-normal">
 						<strong>Hint:</strong>{" "}
 						<span className="">
 							A little something to brighten your day! 🌞
@@ -69,7 +69,7 @@ export default async function Confirm({
 							/>
 						</ConfirmGift>
 
-						<p className="mb-2 mt-5 max-w-[30rem] text-center leading-7 text-base md:text-[1.125rem] md:leading-[1.5] text-default-800 font-normal mx-auto">
+						<p className="mb-2 mt-5 max-w-[30rem] text-center leading-7 text-base md:text-[1.125rem] md:leading-[1.5] text-black font-normal mx-auto">
 							Enter your address{" "}
 							<strong className="hidden sm:inline">securely</strong> to receive
 							your gift! <br />
@@ -81,11 +81,11 @@ export default async function Confirm({
 			<div className="w-full mx-auto flex flex-col items-center justify-center gap-10 my-20 py-12">
 				<Link
 					href="/how-it-works"
-					className="text-white text-center hover:text-secondary transition-colors"
+					className="text-black text-center hover:text-secondary transition-colors"
 				>
 					How it works?
 				</Link>
-				<div className="w-full max-w-screen-2xl grid grid-cols-1 md:grid-cols-3 gap-4 text-white">
+				<div className="w-full max-w-screen-2xl grid grid-cols-1 md:grid-cols-3 gap-4 text-black">
 					<InstructionCard
 						icon={<HeartHandshakeIcon className="text-secondary" />}
 						title="Secure"
@@ -131,13 +131,13 @@ function InstructionCard({
 	return (
 		<div className="flex flex-col relative overflow-hidden h-auto text-foreground box-border outline-none data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 shadow-medium rounded-large transition-transform-background motion-reduce:transition-none border-transparent bg-default-400/10 backdrop-blur-lg backdrop-saturate-[1.8]">
 			<div className="flex p-3 z-10 w-full justify-start items-center shrink-0 overflow-inherit color-inherit subpixel-antialiased rounded-t-large gap-2 pb-0">
-				<div className="flex justify-center p-2 rounded-full items-center bg-primary/80 text-secondary">
+				<div className="flex justify-center p-2 rounded-full items-center bg-black/80 text-white">
 					{icon}
 				</div>
-				<div className="text-base font-semibold">{title}</div>
+				<div className="text-base font-semibold text-black">{title}</div>
 			</div>
 
-			<div className="relative flex w-full p-3 flex-auto flex-col place-content-inherit align-items-inherit h-auto break-words text-left overflow-y-auto subpixel-antialiased [&>p]:font-normal [&>p]:text-base [&>p]:text-default-600">
+			<div className="relative flex w-full p-3 flex-auto flex-col place-content-inherit align-items-inherit h-auto break-words text-left overflow-y-auto subpixel-antialiased [&>p]:font-normal [&>p]:text-base [&>p]:text-black">
 				{description}
 			</div>
 		</div>
