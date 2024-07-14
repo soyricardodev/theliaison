3d2bed7269ff94e4615bbfdbbdb64a7ff3342b77
Ricardo Castro <soyricardodev@proton.me>
2024-07-14 00:25:30 -0400

    feat: add customers table

--- apps/gifting-concierge/src/app/(dashboard)/dashboard/customers/page.tsx
+++ apps/gifting-concierge/src/app/(dashboard)/dashboard/customers/page.tsx
@@ -5,15 +5,21 @@ import {
 	CardHeader,
 	CardTitle,
 } from "@theliaison/ui/card";
+import { Customers } from "./customers";
+import { getCustomers } from "./actions";
 
-export default function CustomersPage() {
+export default async function CustomersPage() {
+	const { customers } = await getCustomers();
+
 	return (
 		<Card>
 			<CardHeader>
 				<CardTitle>Customers</CardTitle>
 				<CardDescription>View all customers and their orders.</CardDescription>
 			</CardHeader>
-			<CardContent></CardContent>
+			<CardContent>
+				<Customers customers={customers} />
+			</CardContent>
 		</Card>
 	);
 }
