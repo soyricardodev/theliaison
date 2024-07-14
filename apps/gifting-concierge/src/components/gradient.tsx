22eb57f7a4bb465c34cf48a89e3d0819e8858e3e
Ricardo Castro <soyricardodev@proton.me>
2024-07-14 18:05:24 -0400

    feat: add url control for bg

--- apps/gifting-concierge/src/components/gradient.tsx
+++ apps/gifting-concierge/src/components/gradient.tsx
@@ -7,10 +7,12 @@ export function Gradient({
 	color1 = "#ff5005",
 	color2 = "#dbba95",
 	color3 = "#d0bce1",
+	url,
 }: {
 	color1?: string;
 	color2?: string;
 	color3?: string;
+	url?: string;
 }) {
 	return (
 		<div className="absolute top-0 left-0 z-10 w-screen h-screen overflow-hidden">
@@ -22,18 +24,22 @@ export function Gradient({
 					overflow: "hidden",
 				}}
 			>
-				<ShaderGradient
-					control="props"
-					color1={color1}
-					color2={color2}
-					color3={color3}
-					zoomOut={false}
-					brightness={3.7}
-					grain="off"
-					animate="on"
-					uSpeed={0.4}
-					cDistance={3.6}
-				/>
+				{url != null ? (
+					<ShaderGradient control="query" urlString={url} />
+				) : (
+					<ShaderGradient
+						control="props"
+						color1={color1}
+						color2={color2}
+						color3={color3}
+						zoomOut={false}
+						brightness={3.7}
+						grain="off"
+						animate="on"
+						uSpeed={0.4}
+						cDistance={3.6}
+					/>
+				)}
 			</ShaderGradientCanvas>
 		</div>
 	);
