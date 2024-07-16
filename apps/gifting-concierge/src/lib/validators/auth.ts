87d0f67e21fadff8a0e4effe546e017fc8316617
Ricardo Castro <soyricardodev@proton.me>
2024-07-15 23:26:08 -0400

    feat: simplified auth schema

--- apps/gifting-concierge/src/lib/validators/auth.ts
+++ apps/gifting-concierge/src/lib/validators/auth.ts
@@ -1,30 +1,14 @@
 import { z } from "zod";
 
-export const signUpSchema = z
-	.object({
-		fullName: z
-			.string()
-			.min(2, { message: "Your name should be at least 2 characters" }),
-		username: z
-			.string()
-			.min(5, { message: "Your username should be at least 5 characters" }),
-		email: z.string().email({ message: "Invalid Email Address" }),
-		password: z
-			.string()
-			.min(8, { message: "Password is too short (min 8 characters)" }),
-		"confirm-pass": z
-			.string()
-			.min(8, { message: "Password is too short (min 8 characters)" }),
-	})
-	.refine(
-		(data) => {
-			if (data["confirm-pass"] !== data.password) {
-				return false;
-			}
-			return true;
-		},
-		{ message: "Password does't match", path: ["confirm-pass"] },
-	);
+export const signUpSchema = z.object({
+	fullName: z
+		.string()
+		.min(2, { message: "Your name should be at least 2 characters" }),
+	email: z.string().email({ message: "Invalid Email Address" }),
+	password: z
+		.string()
+		.min(8, { message: "Password is too short (min 8 characters)" }),
+});
 
 export const simpleSignUpSchema = z.object({
 	email: z.string().email({ message: "Invalid Email Address" }),
