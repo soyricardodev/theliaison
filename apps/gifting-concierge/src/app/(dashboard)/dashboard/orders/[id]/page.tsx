c757c1b2ae6e82fb3ee5222833d2324876103264
Ricardo Castro <soyricardodev@proton.me>
2024-07-17 13:39:30 -0400

    feat: add simple orders query

--- apps/gifting-concierge/src/app/(dashboard)/dashboard/orders/[id]/page.tsx
+++ apps/gifting-concierge/src/app/(dashboard)/dashboard/orders/[id]/page.tsx
@@ -1,7 +1,34 @@
-export default function Component({
+import { createClient } from "~/supabase/server";
+
+export default async function Component({
 	params: { id },
 }: {
 	params: { id: string };
 }) {
-	return <div>Order {id}</div>;
+	const supabase = createClient();
+	const { data, error } = await supabase
+		.from("gifts")
+		.select(`
+			id,
+			type,
+			status,
+			users(id, full_name, email),
+			gift_recipients(id, name),
+			gifts_orders_links(id, link, specs),
+			updated_at,
+			created_at
+		`)
+		.eq("id", id)
+		.single();
+
+	if (error) {
+		console.log(error);
+		return <div>Error</div>;
+	}
+
+	return (
+		<div>
+			<pre>{JSON.stringify(data, null, 2)}</pre>
+		</div>
+	);
 }
