f1a88ea9ee76c2bcfe5964bed15971d1efece2b9
Ricardo Castro <soyricardodev@proton.me>
2024-07-18 16:48:39 -0400

    feat: render gift order data

--- apps/gifting-concierge/src/app/(send)/payment-confirmation/actual-data.tsx
+++ apps/gifting-concierge/src/app/(send)/payment-confirmation/actual-data.tsx
@@ -1,6 +1,8 @@
 "use client";
+import { Input } from "@nextui-org/react";
 import { useFormStore } from "../send-by-link/form-store";
 import { createGiftOrder } from "./actions";
+import { ButtonCopyLink } from "./button-copy-link";
 
 export function ActualData({
 	sessionId,
@@ -18,12 +20,45 @@ export function ActualData({
 		formStore.recipientEmail,
 		formStore.recipientSocialPlatform,
 		formStore.recipientSocialHandle,
-	).then(console.log);
+	);
 
 	return (
-		<div>
-			The data you provided is:
-			<pre>{JSON.stringify(formStore, null, 2)}</pre>
+		<div className="p-4 bg-background flex flex-col gap-4">
+			<p>The data you provided is:</p>
+			<div className="flex flex-col gap-1 bg-neutral-300 p-4 rounded-md">
+				<p>
+					<strong>Gift Link</strong>: {formStore.giftLink}
+				</p>
+				<p>
+					<strong>Gift Specifications</strong>: {formStore.giftSpecifications}
+				</p>
+				<p>
+					<strong>Recipient Name</strong>: {formStore.recipientName}
+				</p>
+				<p>
+					<strong>Recipient Phone Number</strong>:{" "}
+					{formStore.recipientPhoneNumber}
+				</p>
+				<p>
+					<strong>Recipient Email</strong>: {formStore.recipientEmail}
+				</p>
+				<p>
+					<strong>Recipient Social Platform</strong>:{" "}
+					{formStore.recipientSocialPlatform}
+				</p>
+				<p>
+					<strong>Recipient Social Handle</strong>:{" "}
+					{formStore.recipientSocialHandle}
+				</p>
+			</div>
+
+			<div>
+				<p>Your order link is:</p>
+
+				<div>
+					<Input value={formStore.giftLink} endContent={<ButtonCopyLink />} />
+				</div>
+			</div>
 		</div>
 	);
 }
