84878b1b492c57c401b03553fc902f873e35ff26
Ricardo Castro <soyricardodev@proton.me>
2024-07-23 12:27:06 -0400

    add theme provider

--- apps/gifting-concierge/src/components/providers.tsx
+++ apps/gifting-concierge/src/components/providers.tsx
@@ -1,16 +1,23 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import type { ThemeProviderProps } from "next-themes/dist/types";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import { Toaster } from "@theliaison/ui/sonner";
 
 const queryClient = new QueryClient();
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export function Providers({ children, ...props }: ThemeProviderProps) {
 	return (
 		<NextUIProvider>
-			<Toaster />
-			<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+			<QueryClientProvider client={queryClient}>
+				<NextThemesProvider {...props}>
+					<Toaster />
+					{children}
+				</NextThemesProvider>
+			</QueryClientProvider>
 		</NextUIProvider>
 	);
 }
