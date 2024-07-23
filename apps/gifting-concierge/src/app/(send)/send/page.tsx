c8df1055e8cafcf4d871ea19f6a2645802b1639b
Ricardo Castro <soyricardodev@proton.me>
2024-07-23 12:46:01 -0400

    remove duplicated header

--- apps/gifting-concierge/src/app/(send)/send/page.tsx
+++ apps/gifting-concierge/src/app/(send)/send/page.tsx
@@ -1,11 +1,8 @@
-import { Header } from "~/components/header";
 import { SimpleSendGift } from "../../components/gift/simple-send-gift";
 
 export default function SendPage() {
 	return (
 		<div className="w-full mx-auto max-w-screen-2xl px-12">
-			<Header />
-
 			<main className="py-20">
 				<SimpleSendGift />
 			</main>
