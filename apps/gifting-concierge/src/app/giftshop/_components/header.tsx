173b92ff83724b6c42e901db97f847bcee035642
Pedro Fernandez <pefernandez1408@gmail.com>
2024-08-18 23:03:07 -0400

    light and dark mode in gift shop

--- apps/gifting-concierge/src/app/giftshop/_components/header.tsx
+++ apps/gifting-concierge/src/app/giftshop/_components/header.tsx
@@ -5,37 +5,37 @@ import { createClient } from "~/supabase/server";
 import { CartToggle } from "./cart-toggle";
 
 export async function Header() {
-	const supabase = createClient();
-	const {
-		data: { user },
-	} = await supabase.auth.getUser();
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-	return (
-		<header className="w-full mx-auto flex items-center justify-between px-4 py-2 bg-white border-b">
-			<Link href="/" className="flex items-center gap-2">
-				<Image
-					src="/logo-white.webp"
-					alt="The Liaison Logo"
-					width={45}
-					height={45}
-				/>
-				<p>
-					TL <strong>Gifting Concierge</strong>
-				</p>
-			</Link>
+  return (
+    <header className="w-full mx-auto flex items-center justify-between px-4 py-2 bg-black/70 backdrop-blur-3xl supports-[backdrop-filter]:bg-white/30 shadow-sm">
+      <Link href="/" className="flex items-center gap-2">
+        <Image
+          src="/logo-white.webp"
+          alt="The Liaison Logo"
+          width={45}
+          height={45}
+        />
+        <p>
+          TL <strong>Gifting Concierge</strong>
+        </p>
+      </Link>
 
-			<ul className="flex items-center justify-center gap-4">
-				<li>
-					<CartToggle />
-				</li>
-				<li>
-					{user ? (
-						<HeaderNavigation isLoggedIn={true} />
-					) : (
-						<HeaderNavigation isLoggedIn={false} />
-					)}
-				</li>
-			</ul>
-		</header>
-	);
+      <ul className="flex items-center justify-center gap-4">
+        <li>
+          <CartToggle />
+        </li>
+        <li>
+          {user ? (
+            <HeaderNavigation isLoggedIn={true} />
+          ) : (
+            <HeaderNavigation isLoggedIn={false} />
+          )}
+        </li>
+      </ul>
+    </header>
+  );
 }
--- apps/gifting-concierge/src/app/giftshop/_components/products-list.tsx
+++ apps/gifting-concierge/src/app/giftshop/_components/products-list.tsx
@@ -3,57 +3,57 @@ import { ProductButton } from "~/app/components/product-button";
 import { createClient } from "~/supabase/server";
 
 export async function ProductsList() {
-	const supabase = createClient();
+  const supabase = createClient();
 
-	const { data } = await supabase
-		.from("products")
-		.select("id, name, image, prices(unit_amount)")
-		.eq("active", true)
-		.eq("type", "gift");
+  const { data } = await supabase
+    .from("products")
+    .select("id, name, image, prices(unit_amount)")
+    .eq("active", true)
+    .eq("type", "gift");
 
-	return (
-		<div className="grid w-full mx-auto grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-12 items-center place-items-center">
-			{data?.map((product, i) => {
-				const price = Intl.NumberFormat("en-US", {
-					style: "currency",
-					currency: "usd",
-					minimumFractionDigits: 0,
-				}).format((product.prices[0]?.unit_amount ?? 0) / 100);
-				const unitPrice = product.prices[0]?.unit_amount ?? 0;
+  return (
+    <div className="grid w-full mx-auto grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-12 items-center place-items-center">
+      {data?.map((product, i) => {
+        const price = Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "usd",
+          minimumFractionDigits: 0,
+        }).format((product.prices[0]?.unit_amount ?? 0) / 100);
+        const unitPrice = product.prices[0]?.unit_amount ?? 0;
 
-				return (
-					<article
-						key={`${product.id}-${i}`}
-						className="relative flex w-64 max-w-full flex-none snap-start scroll-ml-6 flex-col gap-3 rounded-none bg-white/70 backdrop-blur-2xl p-4 shadow-none h-auto"
-					>
-						<div className="rounded-medium bg-content2 relative flex h-52 max-h-full w-full flex-col items-center justify-center overflow-visible">
-							<Image
-								className="w-[300px] h-full object-contain object-center"
-								src={product.image ?? ""}
-								width={300}
-								height={300}
-								alt={product.name ?? ""}
-							/>
-						</div>
-						<div className="flex flex-col gap-3 px-1">
-							<div className="flex items-center justify-between">
-								<h3 className="text-medium text-default-700 font-medium">
-									{product.name}
-								</h3>
-								<p className="text-medium text-default-500 font-medium">
-									{price}
-								</p>
-							</div>
-							<div className="flex gap-2">
-								<ProductButton
-									productId={product.id}
-									productUnitPrice={unitPrice}
-								/>
-							</div>
-						</div>
-					</article>
-				);
-			})}
-		</div>
-	);
+        return (
+          <article
+            key={`${product.id}-${i}`}
+            className="relative flex w-64 max-w-full flex-none snap-start scroll-ml-6 flex-col gap-3 rounded-md bg-white/70 backdrop-blur-2xl p-4 shadow-none h-auto"
+          >
+            <div className="rounded-medium bg-content2 relative flex h-52 max-h-full w-full flex-col items-center justify-center overflow-visible">
+              <Image
+                className="w-[300px] h-full object-contain object-center"
+                src={product.image ?? ""}
+                width={300}
+                height={300}
+                alt={product.name ?? ""}
+              />
+            </div>
+            <div className="flex flex-col gap-3 px-1">
+              <div className="flex items-center justify-between">
+                <h3 className="text-medium text-default-700 font-medium">
+                  {product.name}
+                </h3>
+                <p className="text-medium text-foreground/60 font-medium">
+                  {price}
+                </p>
+              </div>
+              <div className="flex gap-2">
+                <ProductButton
+                  productId={product.id}
+                  productUnitPrice={unitPrice}
+                />
+              </div>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
 }
